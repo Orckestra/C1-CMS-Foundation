7dbd3cf97d5b5087e1e5600ecc6d8039bc36d5a4
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2010-07-21 14:30:55 +0200

    Refactoring for a workaround used in Logger+WCF so it is reusable

---
+++ CompositeApplications/WebClient/WCF/ServiceHostFactoryHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Composite.WebClient.WCF
+{
+    public static class ServiceHostFactoryHelper 
+    {
+        public static Uri[] GetFixedBaseAddresses(Uri[]  baseAddresses)
+        {
+            // Parsing url set like
+            //
+            //   http://local/Composite/services/LogService/local_80/LogService
+            //   http://domain/Composite/services/LogService/local_80/LogService
+            //   http://local:32/Composite/services/LogService/local_80/LogService
+            //
+            // In the example the first url has much for hostname: "//local/" and "local_80", so have to be choosen as a base url
+
+
+            var newAddresses = new List<Uri>();
+
+            foreach (Uri uri in baseAddresses)
+            {
+
+                string query = baseAddresses[0].PathAndQuery;
+                int offset1 = query.LastIndexOf("/");
+                int offset2 = query.LastIndexOf("/", offset1 - 1);
+
+                string hostNameAndPort = query.Substring(offset2 + 1, offset1 - offset2 - 1);
+                int separatorIndex = hostNameAndPort.LastIndexOf("_");
+
+                if (separatorIndex > 0)
+                {
+                    int port;
+                    string hostName = hostNameAndPort.Substring(0, separatorIndex);
+
+                    if (int.TryParse(hostNameAndPort.Substring(separatorIndex + 1), out port)
+                        && uri.Host == hostName && uri.Port == port)
+                    {
+                        newAddresses.Add(uri);
+                    }
+                }
+            }
+
+            if (newAddresses.Count == 0)
+            {
+                newAddresses.Add(baseAddresses[0]);
+            }
+
+            return newAddresses.ToArray();
+        }
+    }
+}
