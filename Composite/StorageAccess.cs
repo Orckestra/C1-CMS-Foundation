286220120af627bdcb43e6cd8e748fddf56b964c
Marcus Wendt <marcus@wendt.dk>
2010-08-27 10:48:00 +0200

    Added help info

--- Composite/StorageAccess.cs
+++ Composite/StorageAccess.cs
@@ -10,23 +10,95 @@ namespace Composite.Data
 {
     public class C1DataConnection : IDisposable
     {
+        /// <summary>
+        /// Creates a new <see cref="C1DataConnection"/> instance that can be used to access data stored in Composite C1.
+        /// </summary>
+        /// <example>
+        /// Here is an example of how to use <see cref="C1DataConnection" />
+        /// <code>
+        /// using (C1DataConnection connection = new C1DataConnection())
+        /// {
+        ///    var q = 
+        ///       from d in connection.Get&lt;IMyDataType&gt;()
+        ///       where d.Name == "Foo"
+        ///       select d;
+        /// }
+        /// </code>
+        /// </example>
         public C1DataConnection()
             : this(PublicationScope.Public, null)
         {
+#warning WE SHOULD NOT SWITCH TO PUBLIC - WE SHOULD CONTINUE WITH WHAT IS - OR DEFAULT TO PUBLIC
         }
 
+        /// <summary>
+        /// Creates a new <see cref="C1DataConnection"/> instance that can be used to access data stored in Composite C1.
+        /// </summary>
+        /// <example>
+        /// Here is an example of how to use <see cref="C1DataConnection" />
+        /// In this example the data items returned by the <see cref="C1DataConnection"/> is from the
+        /// internal scope - data that has not yet been published.
+        /// <code>
+        /// using (C1DataConnection connection = new C1DataConnection(PublicationScope.Internal))
+        /// {
+        ///    var q = 
+        ///       from d in connection.Get&lt;IMyDataType&gt;()
+        ///       where d.Name == "Foo"
+        ///       select d;
+        /// }
+        /// </code>
+        /// </example>
+        /// <param name="publicationScope">The desired <see cref="PublicationScope"/></param>
         public C1DataConnection(PublicationScope scope)
             : this(scope, null)
         {
         }
 
 
+        /// <summary>
+        /// Creates a new <see cref="C1DataConnection"/> instance that can be used to access data stored in Composite C1.
+        /// </summary>        
+        /// <example>
+        /// Here is an example of how to use <see cref="C1DataConnection" />
+        /// In this example the data items returned by the <see cref="C1DataConnection"/> is from the
+        /// Danish locale - data for the Danish website.
+        /// <code>
+        /// using (C1DataConnection connection = new C1DataConnection(new CultureInfo("da-DK")))
+        /// {
+        ///    var q = 
+        ///       from d in connection.Get&lt;IMyDataType&gt;()
+        ///       where d.Name == "Foo"
+        ///       select d;
+        /// }
+        /// </code>
+        /// </example>
+        /// <param name="locale">The desired locale</param>
         public C1DataConnection(CultureInfo locale)
             : this(PublicationScope.Public, locale)
         {
+#warning WE SHOULD NOT SWITCH TO PUBLIC - WE SHOULD CONTINUE WITH WHAT IS - OR DEFAULT TO PUBLIC
         }
 
 
+        /// <summary>
+        /// Creates a new <see cref="C1DataConnection"/> instance that can be used to access data stored in Composite C1.
+        /// </summary>        
+        /// <example>
+        /// Here is an example of how to use <see cref="C1DataConnection" />
+        /// In this example the data items returned by the <see cref="C1DataConnection"/> is from the
+        /// internal scope and the Danish locale - unpublished data for the Danish website.
+        /// <code>
+        /// using (C1DataConnection connection = new C1DataConnection(PublicationScope.Internal, new CultureInfo("da-DK")))
+        /// {
+        ///    var q = 
+        ///       from d in connection.Get&lt;IMyDataType&gt;()
+        ///       where d.Name == "Foo"
+        ///       select d;
+        /// }
+        /// </code>
+        /// </example>
+        /// <param name="publicationScope">The desired <see cref="PublicationScope"/></param>
+        /// <param name="locale">The desired locale</param>
         public C1DataConnection(PublicationScope scope, CultureInfo locale)
         {
             this.PublicationScope = scope;
