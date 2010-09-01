f63455496b9cccfdabb6c7b42c66dc9a7f512bd3
Martin Ingvar Kofoed Jensen <ingvar@netaben.dk>
2010-09-01 08:19:29 +0200

    Small IPageFolderData and IPageMetaData fix

--- Composite/Data/IPageFolderData.cs
+++ Composite/Data/IPageFolderData.cs
@@ -7,7 +7,7 @@ namespace Composite.Data
     /// <exclude />
     [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
     [DataAssociationAttribute(typeof(Composite.Data.Types.IPage), "PageId", DataAssociationType.Aggregation)]
-    public interface IPageFolderData : IData
+    public interface IPageFolderData : IPageData
     {
     }
 }
--- Composite/Data/IPageMetaData.cs
+++ Composite/Data/IPageMetaData.cs
@@ -6,7 +6,7 @@ namespace Composite.Data
     /// <exclude />
     [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
     [DataAssociationAttribute(typeof(Composite.Data.Types.IPage), "PageId", DataAssociationType.Composition)]
-    public interface IPageMetaData : IData
+    public interface IPageMetaData : IPageData
     {
         [StoreFieldType(PhysicalStoreFieldType.String, 128)]
         [ImmutableFieldId("{B2A5EE23-848D-4D0B-A801-FDF68F9F899E}")]
