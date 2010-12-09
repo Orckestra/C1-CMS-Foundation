88a741d45c640cd8d27196b8bbf7ef6663c8827c
Martin Ingvar Kofoed Jensen <ingvar@netaben.dk>
2010-12-09 14:21:29 +0100

    Adding AzureBootLoader projects

--- Composite/Plugins/IO/IOProviders/LocalIOProvider/LocalC1StreamReader.cs
+++ Composite/Plugins/IO/IOProviders/LocalIOProvider/LocalC1StreamReader.cs
@@ -26,7 +26,7 @@ namespace Composite.Plugins.IO.IOProviders.LocalIOProvider
 
 
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Composite.IO", "Composite.DotNotUseStreamReaderClass:DotNotUseStreamReaderClass")]
+        [System.Diagnos tics.CodeAnalysis.SuppressMessage("Composite.IO", "Composite.DotNotUseStreamReaderClass:DotNotUseStreamReaderClass")]
         public int Read()
         {
             return _streamReader.Read();
