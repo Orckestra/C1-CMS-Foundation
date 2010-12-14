76f91b36bed2b46e9b7d1048edc1e26d5132badd
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2010-12-14 15:01:52 +0100

    Updating version to 2.1 (beta 2)

--- Composite/Properties/AssemblyInfo.cs
+++ Composite/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Composite C1 2.1 (BETA 1)")]
+[assembly: AssemblyTitle("Composite C1 2.1 (BETA 2)")]
 [assembly: AssemblyDescription("Composite C1 Core classes")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Composite A/S")]
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyVersion("2.1.*")]
 
 [assembly: InternalsVisibleTo("UpgradePackage")]
