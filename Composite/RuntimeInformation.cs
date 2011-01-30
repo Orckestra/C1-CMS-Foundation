b84ea2ddfc50f22ebda908f7ab9ca4460e4eb6fe
Marcus Wendt <marcus@wendt.dk>
2011-01-30 02:32:49 +0100

    Backtracking on the About info on 'signed vs. open source code'. It's *hell* to locate signature from signtool.exe, not supported by .net framework.

--- Composite/RuntimeInformation.cs
+++ Composite/RuntimeInformation.cs
@@ -88,17 +88,7 @@ namespace Composite
                 Assembly asm = typeof(RuntimeInformation).Assembly;
                 string assemblyTitle = ((AssemblyTitleAttribute)asm.GetCustomAttributes(typeof(AssemblyTitleAttribute), false)[0]).Title;
 
-                byte[] key = asm.GetName().GetPublicKey();
-                bool isSignedAsm = key.Length > 0;
-
-                if (isSignedAsm)
-                {
-                    return assemblyTitle;
-                }
-                else
-                {
-                    return string.Format("{0} compiled from open source.", assemblyTitle);
-                }
+                return assemblyTitle;
             }
         }
 
