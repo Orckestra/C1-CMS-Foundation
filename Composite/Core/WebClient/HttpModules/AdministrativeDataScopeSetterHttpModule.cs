4ab17302f1da736929e4887ba77cd44d77588bd6
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2011-03-03 15:56:52 +0100

    Fixing 3388. "Package unregistartion failed when uninstalling packages"

--- Composite/Core/WebClient/HttpModules/AdministrativeDataScopeSetterHttpModule.cs
+++ Composite/Core/WebClient/HttpModules/AdministrativeDataScopeSetterHttpModule.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Web;
 using Composite.C1Console.Security;
-using Composite.Core.Logging;
 using Composite.Data;
 using Composite.C1Console.Users;
 using Composite.Core.Configuration;
@@ -16,46 +12,39 @@ namespace Composite.Core.WebClient.HttpModules
     {
         public void Init(HttpApplication context)
         {
-            context.AuthenticateRequest += new EventHandler(context_AuthenticateRequest);
-            context.EndRequest += new EventHandler(context_EndRequest);
+            var moduleInstance = new ModuleInstance();
+            context.AuthenticateRequest += moduleInstance.AuthenticateRequest;
+            context.EndRequest += moduleInstance.EndRequest;
         }
 
-
-
-        void context_AuthenticateRequest(object sender, EventArgs e)
+        private class ModuleInstance
         {
-            if (SystemSetupFacade.IsSystemFirstTimeInitialized == false) return;
+            private DataScope _dataScope;
 
-            HttpApplication application = (HttpApplication)sender;
-            HttpContext context = application.Context;
-
-            bool adminRootRequest = context.Request.Path.ToLower().StartsWith(UrlUtils.AdminRootPath.ToLower());
-
-            if (adminRootRequest == true && UserValidationFacade.IsLoggedIn() == true)
+            public void AuthenticateRequest(object sender, EventArgs e)
             {
-                context.Items.Add("AdministrativeDataScopeSetterHttpModule.DataScope", new DataScope(DataScopeIdentifier.Administrated, UserSettings.ActiveLocaleCultureInfo));
+                if (SystemSetupFacade.IsSystemFirstTimeInitialized == false) return;
+
+                HttpApplication application = (HttpApplication)sender;
+                HttpContext context = application.Context;
+
+                bool adminRootRequest = context.Request.Path.ToLower().StartsWith(UrlUtils.AdminRootPath.ToLower());
+
+                if (adminRootRequest == true && UserValidationFacade.IsLoggedIn() == true)
+                {
+                    _dataScope = new DataScope(DataScopeIdentifier.Administrated, UserSettings.ActiveLocaleCultureInfo);
+                }
+            }
+
+            public void EndRequest(object sender, EventArgs e)
+            {
+                if (_dataScope != null)
+                {
+                    _dataScope.Dispose();
+                }
             }
         }
 
-
-
-        void context_EndRequest(object sender, EventArgs e)
-        {
-            if (SystemSetupFacade.IsSystemFirstTimeInitialized == false) return;
-
-            HttpApplication application = (HttpApplication)sender;
-            HttpContext context = application.Context;
-
-            if (context.Items.Contains("AdministrativeDataScopeSetterHttpModule.DataScope") == true)
-            {
-                DataScope dataScope = (DataScope)context.Items["AdministrativeDataScopeSetterHttpModule.DataScope"];
-
-                dataScope.Dispose();
-            }
-        }
-
-        
-        
         public void Dispose()
         {
         }
