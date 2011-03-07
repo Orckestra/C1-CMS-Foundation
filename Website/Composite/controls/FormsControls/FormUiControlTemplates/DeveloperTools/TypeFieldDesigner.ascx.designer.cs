f84bbdee9e2b87f190c5b018750b9319692ac505
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2011-03-07 10:13:06 +0100

    Fixing the build errors

--- Website/Composite/controls/FormsControls/FormUiControlTemplates/DeveloperTools/TypeFieldDesigner.ascx.designer.cs
+++ Website/Composite/controls/FormsControls/FormUiControlTemplates/DeveloperTools/TypeFieldDesigner.ascx.designer.cs
@@ -139,6 +139,15 @@ namespace CompositeTypeFieldDesigner {
         protected global::Composite.Core.WebClient.UiControlLib.Selector TypeDetailsSelector;
         
         /// <summary>
+        /// TypeDetailsOptionalPlaceHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.PlaceHolder TypeDetailsOptionalPlaceHolder;
+        
+        /// <summary>
         /// OptionalSelector control.
         /// </summary>
         /// <remarks>
