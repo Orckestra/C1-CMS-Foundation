92f595eccc6bc530a2830e14b5aea59f2e6b2213
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2011-03-09 13:33:07 +0100

    Changing version to "2.1"

--- Composite.Workflows/Properties/AssemblyInfo.cs
+++ Composite.Workflows/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Composite.Workflows")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyCopyright("Copyright © Composite A/S 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
