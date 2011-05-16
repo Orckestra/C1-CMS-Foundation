1086c8b1cdcc14070cdcb9cbed97d951595421d3
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2011-05-16 15:52:00 +0200

    Making C1 page routing compatible with MVC routing

--- Composite/Core/Routing/Routes.cs
+++ Composite/Core/Routing/Routes.cs
@@ -7,7 +7,8 @@ namespace Composite.Core.Routing
     {
         public static void Register()
         {
-            RouteTable.Routes.Add(new C1PageRoute());
+            // Inserting to the first position, so it is executed before MVC route
+            RouteTable.Routes.Insert(0, new C1PageRoute());
         }
     }
 }
