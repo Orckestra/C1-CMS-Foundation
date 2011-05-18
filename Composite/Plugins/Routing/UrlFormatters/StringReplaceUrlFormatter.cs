0163383ea107710403902c073c7fd1f6b8a092a7
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2011-05-18 16:04:55 +0200

    Updating configuration structure for urls

--- Composite/Plugins/Routing/UrlFormatters/StringReplaceUrlFormatter.cs
+++ Composite/Plugins/Routing/UrlFormatters/StringReplaceUrlFormatter.cs
@@ -1,9 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Web.Hosting;
+using System.Xml.Linq;
+using Composite.Core.Extensions;
+using Composite.Core.IO;
 using Composite.Core.Routing.Plugins.UrlFormatters;
 using Composite.Core.Routing.Plugins.UrlFormatters.Runtime;
 using Composite.Core.Types;
+using Composite.Core.Xml;
 using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
 using Microsoft.Practices.EnterpriseLibrary.Common.Configuration.ObjectBuilder;
 using Microsoft.Practices.ObjectBuilder;
@@ -40,6 +47,22 @@ namespace Composite.Plugins.Routing.UrlFormatters
     [Assembler(typeof(StringReplaceUrlFormatterAssembler))]
     internal sealed class StringReplaceUrlFormatterData : UrlFormatterData
     {
+        private const string RulesFilePropertyName = "rulesFile";
+
+        /// <exclude />
+        [ConfigurationProperty(RulesFilePropertyName, IsRequired = false)]
+        public string RulesFile
+        {
+            get
+            {
+                return (string)this[RulesFilePropertyName];
+            }
+            set
+            {
+                this[RulesFilePropertyName] = value;
+            }
+        }
+
         private const string ReplaceProperty = "ReplacementRules";
         [ConfigurationProperty(ReplaceProperty)]
         public ReplacementElementCollection Replace
@@ -75,13 +98,39 @@ namespace Composite.Plugins.Routing.UrlFormatters
         {
             var data = (StringReplaceUrlFormatterData)objectConfiguration;
 
+            string rulesFile = data.RulesFile;
+            if (!rulesFile.IsNullOrEmpty())
+            {
+                string fullPath = HostingEnvironment.MapPath(rulesFile);
+                Verify.That(C1File.Exists(fullPath), "Cannot find file '{0}'", rulesFile);
+
+                try
+                {
+                    XDocument xDoc = XDocumentUtils.Load(fullPath);
+
+                    return new StringReplaceUrlFormatter(
+                        xDoc.Root.Elements()
+                        .Select(e => new Pair<string, string>(GetAttributeNotNull(e, "oldValue"), GetAttributeNotNull(e, "newValue"))).ToList());
+                }
+                catch (Exception e)
+                {
+                    throw new ConfigurationErrorsException("Failed to process file '{0}'".FormatWith(rulesFile), e);
+                }
+            }
+
             var replacements = data.Replace.Cast<ReplacementRuleConfigurationElement>();
 
             return new StringReplaceUrlFormatter(replacements.Select(r => new Pair<string, string>(r.OldValue, r.NewValue)).ToList());
         }
+
+        private static string GetAttributeNotNull(XElement xelement, string attributeName)
+        {
+            var attr = xelement.Attribute(attributeName);
+            Verify.IsNotNull(attr, "Missing '{0}' attribute", attributeName);
+
+            return attr.Value;
+        }
     }
-
-
     internal sealed class ReplacementRuleConfigurationElement : ConfigurationElement
     {
         private const string _oldValueAttributeName = "oldValue";
