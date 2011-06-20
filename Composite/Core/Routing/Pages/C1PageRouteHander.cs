ee343193f48f7d90ac2e2c802377518a5c8a47f2
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2011-06-20 10:15:49 +0200

    Implementation for /Renderers/Page.aspx shouldn't be hardcoded in C1PageRoute

--- Composite/Core/Routing/Pages/C1PageRouteHander.cs
+++ Composite/Core/Routing/Pages/C1PageRouteHander.cs
@@ -1,4 +1,8 @@
-﻿using System.Web;
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Compilation;
+using System.Web.Configuration;
 using System.Web.Routing;
 using System.Web.UI;
 
@@ -6,9 +10,32 @@ namespace Composite.Core.Routing.Pages
 {
     internal class C1PageRouteHandler : IRouteHandler
     {
+        private static readonly Type _handlerType;
+
+        static C1PageRouteHandler()
+        {
+            string sectionName = HttpRuntime.UsingIntegratedPipeline ? "system.webServer/handlers" : "system.web/httpHandlers";
+            var section = (HttpHandlersSection)WebConfigurationManager.GetWebApplicationSection(sectionName);
+
+            if(section != null)
+            {
+                var item = section.Handlers.Cast<HttpHandlerAction>().SingleOrDefault(a => String.Equals(a.Path, "Renderers/Page.aspx", StringComparison.OrdinalIgnoreCase));
+                if (item != null)
+                {
+                    _handlerType = Type.GetType(item.Type);
+                }
+            }
+        }
+
+
         public IHttpHandler GetHttpHandler(RequestContext requestContext)
         {
-            return System.Web.Compilation.BuildManager.CreateInstanceFromVirtualPath("~/Renderers/Page.aspx", typeof(Page)) as Page;
+            if (_handlerType != null)
+            {
+                return (IHttpHandler)Activator.CreateInstance(_handlerType);
+            }
+            
+            return (IHttpHandler)BuildManager.CreateInstanceFromVirtualPath("~/Renderers/Page.aspx", typeof(Page));
         }
     }
 }
