21f1c0aa4619fb6cd079351da9ee057e9550aa22
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2011-07-01 14:34:16 +0200

    Changing version to 2.1.3 (BETA 2)

--- Composite/Properties/AssemblyInfo.cs
+++ Composite/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 #if !InternalBuild
-[assembly: AssemblyTitle("Composite C1 2.1.3 (BETA)")]
+[assembly: AssemblyTitle("Composite C1 2.1.3 (BETA 2)")]
 #else
-[assembly: AssemblyTitle("Composite C1 2.1.3 (Internal build)")]
+[assembly: AssemblyTitle("Composite C1 2.1.3 (BETA 2 Internal build)")]
 #endif
 
 [assembly: AssemblyDescription("Composite C1 Core classes")]
