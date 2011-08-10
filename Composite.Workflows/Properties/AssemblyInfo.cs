995a8bc0b072b14c13c10e403d816ff5893495fb
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2011-08-10 15:15:59 +0200

    Disabling mergin 2 dlls into one

--- Composite.Workflows/Properties/AssemblyInfo.cs
+++ Composite.Workflows/Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Composite.Workflows")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Composite.Workflows")]
+[assembly: AssemblyCompany("Composite A/S")]
+[assembly: AssemblyProduct("Composite C1")]
 [assembly: AssemblyCopyright("Copyright © Composite A/S 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,7 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.1.*")]
 
 [assembly: InternalsVisibleTo("UpgradePackage")]
