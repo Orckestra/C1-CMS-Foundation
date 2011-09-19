71b59a7165fa7ebdf3334f299f013ab872e57da7
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2011-09-19 15:56:29 +0200

    Making more stuff public

--- Composite/Core/ResourceSystem/Icons/CommonCommandIcons.cs
+++ Composite/Core/ResourceSystem/Icons/CommonCommandIcons.cs
@@ -1,23 +1,31 @@
 namespace Composite.Core.ResourceSystem.Icons
 {
-    internal static class CommonCommandIcons
+    /// <exclude />
+    [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)] 
+    public static class CommonCommandIcons
     {
+        /// <exclude />
         public static ResourceHandle AddNew { get { return GetIconHandle("generic-add"); } }
+        /// <exclude />
         public static ResourceHandle Edit { get { return GetIconHandle("generic-edit"); } }
+        /// <exclude />
         public static ResourceHandle Refresh { get { return GetIconHandle("generic-refresh"); } }
+        /// <exclude />
         public static ResourceHandle Delete { get { return GetIconHandle("generic-delete"); } }
+        /// <exclude />
         public static ResourceHandle Search { get { return GetIconHandle("generic-search"); } }
+        /// <exclude />
         public static ResourceHandle SetSecurity { get { return GetIconHandle("generic-set-security"); } }
+        /// <exclude />
         public static ResourceHandle ShowHistory { get { return GetIconHandle("generic-show-history"); } }
+        /// <exclude />
         public static ResourceHandle ShowReport { get { return GetIconHandle("generic-show-report"); } }
 
 
-
-        private static ResourceHandle GetIconHandle()
-        {
-            return new ResourceHandle(BuildInIconProviderName.ProviderName, "unknown");
-        }
-
+        //private static ResourceHandle GetIconHandle()
+        //{
+        //    return new ResourceHandle(BuildInIconProviderName.ProviderName, "unknown");
+        //}
 
         private static ResourceHandle GetIconHandle(string name)
         {
