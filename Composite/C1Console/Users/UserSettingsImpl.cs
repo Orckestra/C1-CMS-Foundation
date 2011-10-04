45ec7018ad5c14a19f543152de431187643e8105
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2011-10-04 10:22:15 +0200

    Fixing 3594: "An exception during the setup when specifying the admin name with uppercase letters"

--- Composite/C1Console/Users/UserSettingsImpl.cs
+++ Composite/C1Console/Users/UserSettingsImpl.cs
@@ -301,7 +301,7 @@ namespace Composite.C1Console.Users
         {
             IUserSettings settings = DataFacade.BuildNew<IUserSettings>();
 
-            settings.Username = username;
+            settings.Username = username.ToLowerInvariant();
             settings.CultureName = GlobalSettingsFacade.DefaultCultureName;
 
             return DataFacade.AddNew<IUserSettings>(settings);
