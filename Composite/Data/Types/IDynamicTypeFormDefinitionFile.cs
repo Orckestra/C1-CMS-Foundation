e0fbf9b19d19c6497aff24558ba43bbc687ae2aa
Marcus Wendt <marcus@wendt.dk>
2011-11-09 13:44:43 +0100

    Fixing 3710 - could not save in data type "Edit Form Markup" workflow on new types. Added immuteable type id to IFile type in question.

--- Composite/Data/Types/IDynamicTypeFormDefinitionFile.cs
+++ Composite/Data/Types/IDynamicTypeFormDefinitionFile.cs
@@ -5,6 +5,7 @@
     /// <exclude />
     [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)] 
     [DataScope(DataScopeIdentifier.PublicName)]
+    [ImmutableTypeId("{42f4ca9a-b18b-4d62-a4f5-5f04c06109ed}")]
     public interface IDynamicTypeFormDefinitionFile : IFile
     {
     }
