b7653f883ef50acc8b1f0f9cb2203b16c2a12eda
Marcus Wendt <marcus@wendt.dk>
2012-01-04 17:17:34 +0100

    Fixing issue where enabling cookieless sessions kill icons in the C1 Console

--- Website/Composite/scripts/source/top/ui/bindings/labels/LabelBinding.js
+++ Website/Composite/scripts/source/top/ui/bindings/labels/LabelBinding.js
@@ -185,7 +185,7 @@ LabelBinding.prototype.setAlphaTransparentBackdrop = function ( url ) {
 			if ( Client.isExplorer6 ) {
 				this.shadowTree.labelBody.style.filter = LabelBinding.EXPLORER_IMAGE_FILTER.replace ( "${url}", url );
 			} else {
-				this.shadowTree.labelBody.style.backgroundImage = "url(" + url + ")";
+				this.shadowTree.labelBody.style.backgroundImage = "url('" + url + "')";
 			}
 		} else {
 			if ( Client.isExplorer6 ) {
