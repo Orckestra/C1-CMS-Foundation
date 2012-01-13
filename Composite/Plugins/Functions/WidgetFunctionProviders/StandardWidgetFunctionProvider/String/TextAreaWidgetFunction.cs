c8b0f2d725001ae65c17a56bbabd86950282be62
Marcus Wendt <marcus@wendt.dk>
2012-01-13 15:43:06 +0100

    Widget TextArea og TextBox now support SpellCheck attribute

--- Composite/Plugins/Functions/WidgetFunctionProviders/StandardWidgetFunctionProvider/String/TextAreaWidgetFunction.cs
+++ Composite/Plugins/Functions/WidgetFunctionProviders/StandardWidgetFunctionProvider/String/TextAreaWidgetFunction.cs
@@ -11,19 +11,35 @@ namespace Composite.Plugins.Functions.WidgetFunctionProviders.StandardWidgetFunc
     internal sealed class TextAreaWidgetFuntion : CompositeWidgetFunctionBase
     {
         private const string _functionName = "TextArea";
+
+        /// <exclude />
         public const string CompositeName = CompositeWidgetFunctionBase.CommonNamespace + ".String." + _functionName;
+
+        /// <exclude />
+        public const string SpellCheckParameterName = "SpellCheck";
 
 
         public TextAreaWidgetFuntion(EntityTokenFactory entityTokenFactory)
             : base(CompositeName, typeof(string), entityTokenFactory)
         {
+            ParameterProfile spellCheckPP =
+                new ParameterProfile(TextAreaWidgetFuntion.SpellCheckParameterName,
+                    typeof(bool), false,
+                    new ConstantValueProvider(true), StandardWidgetFunctions.GetBoolSelectorWidget("Allow spell checking","Do not allow spell checking"), null,
+                    "Spell check", new HelpDefinition("By default text will be spell checked (when available). You should explicitly disable spell checking on fields that contain e-mails, code values etc. not suitable for spell checking. "));
+
+            base.AddParameterProfile(spellCheckPP);
         }
 
 
 
         public override XElement GetWidgetMarkup(ParameterList parameters, string label, HelpDefinition help, string bindingSourceName)
         {
-            return base.BuildBasicWidgetMarkup("TextArea", "Text", label, help, bindingSourceName);
+            bool spellCheck = parameters.GetParameter<bool>(TextAreaWidgetFuntion.SpellCheckParameterName);
+
+            XElement formElement = base.BuildBasicWidgetMarkup("TextArea", "Text", label, help, bindingSourceName);
+            formElement.Add(new XAttribute("SpellCheck", spellCheck));
+            return formElement;
         }
     }
 }
--- Composite/Plugins/Functions/WidgetFunctionProviders/StandardWidgetFunctionProvider/String/TextBoxWidgetFuntion.cs
+++ Composite/Plugins/Functions/WidgetFunctionProviders/StandardWidgetFunctionProvider/String/TextBoxWidgetFuntion.cs
@@ -11,19 +11,34 @@ namespace Composite.Plugins.Functions.WidgetFunctionProviders.StandardWidgetFunc
 	internal sealed class TextBoxWidgetFuntion : CompositeWidgetFunctionBase
     {
         private const string _functionName = "TextBox";
-        public const string CompositeName = CompositeWidgetFunctionBase.CommonNamespace + ".String." +_functionName;
 
+        /// <exclude />
+        public const string CompositeName = CompositeWidgetFunctionBase.CommonNamespace + ".String." + _functionName;
+
+        /// <exclude />
+        public const string SpellCheckParameterName = "SpellCheck";
 
         public TextBoxWidgetFuntion(EntityTokenFactory entityTokenFactory)
             : base(CompositeName, typeof(string), entityTokenFactory)
         {
+            ParameterProfile spellCheckPP =
+                new ParameterProfile(TextBoxWidgetFuntion.SpellCheckParameterName,
+                    typeof(bool), false,
+                    new ConstantValueProvider(true), StandardWidgetFunctions.GetBoolSelectorWidget("Allow spell checking", "Do not allow spell checking"), null,
+                    "Spell check", new HelpDefinition("By default text will be spell checked (when available). You should explicitly disable spell checking on fields that contain e-mails, code values etc. not suitable for spell checking. "));
+
+            base.AddParameterProfile(spellCheckPP);
         }
 
 
 
         public override XElement GetWidgetMarkup(ParameterList parameters, string label, HelpDefinition help, string bindingSourceName)
         {
-            return base.BuildBasicWidgetMarkup("TextBox", "Text", label, help, bindingSourceName);
+            bool spellCheck = parameters.GetParameter<bool>(TextBoxWidgetFuntion.SpellCheckParameterName);
+
+            XElement formElement = base.BuildBasicWidgetMarkup("TextBox", "Text", label, help, bindingSourceName);
+            formElement.Add(new XAttribute("SpellCheck", spellCheck));
+            return formElement;
         }
     }
 }
