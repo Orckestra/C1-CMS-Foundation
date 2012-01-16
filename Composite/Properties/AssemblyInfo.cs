579cf772154fca1a4fd8b00c51663256c11aef7d
Marcus Wendt <marcus@wendt.dk>
2012-01-16 22:44:22 +0100

    This build is now refered to "3.1" for easier referense, immediately visible in version number.

--- Composite/Properties/AssemblyInfo.cs
+++ Composite/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 #if !InternalBuild
-[assembly: AssemblyTitle("Composite C1 3.0.1")]
+[assembly: AssemblyTitle("Composite C1 3.1")]
 #else
-[assembly: AssemblyTitle("Composite C1 3.0.1 (Internal build)")]
+[assembly: AssemblyTitle("Composite C1 3.1 (Internal build)")]
 #endif
 
 [assembly: AssemblyDescription("Composite C1 Core classes")]
@@ -39,7 +39,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.0.*")]
+[assembly: AssemblyVersion("3.1.*")]
 
 [assembly: InternalsVisibleTo("UpgradePackage")]
 [assembly: InternalsVisibleTo("Composite.Workflows")]
