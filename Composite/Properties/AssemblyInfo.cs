94804c1ee847aeb80d687db7f64811f58b182f2b
Marcus Wendt <marcus@wendt.dk>
2012-02-02 15:58:06 +0100

    Bumped the release number to 3.2

--- Composite/Properties/AssemblyInfo.cs
+++ Composite/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 #if !InternalBuild
-[assembly: AssemblyTitle("Composite C1 3.1")]
+[assembly: AssemblyTitle("Composite C1 3.2")]
 #else
-[assembly: AssemblyTitle("Composite C1 3.1 (Internal build)")]
+[assembly: AssemblyTitle("Composite C1 3.2 (Internal build)")]
 #endif
 
 [assembly: AssemblyDescription("Composite C1 Core classes")]
