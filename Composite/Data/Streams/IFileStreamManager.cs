f04aa4e20f2b80c67c62a5c4191b554eea9aa078
Martin Ingvar Kofoed Jensen <ingvar@netaben.dk>
2012-02-02 10:48:17 +0100

    Making IFileStreamManager public

--- Composite/Data/Streams/IFileStreamManager.cs
+++ Composite/Data/Streams/IFileStreamManager.cs
@@ -32,10 +32,28 @@ namespace Composite.Data.Streams
 
 
 
-    internal interface IFileStreamManager
+    /// <summary>
+    /// </summary>
+    public interface IFileStreamManager
     {
+        /// <summary>
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
         Stream GetReadStream(IFile file);
+
+
+        /// <summary>
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
         Stream GetNewWriteStream(IFile file);
+
+
+        /// <summary>
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="handler"></param>
         void SubscribeOnFileChanged(IFile file, OnFileChangedDelegate handler);
     }
 }
