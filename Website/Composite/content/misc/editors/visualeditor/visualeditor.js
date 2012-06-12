cfa583d94e93008cbe51be349a74ebcd866bd201
Taras Nakonechnyi <neexite@gmail.com>
2012-06-12 10:46:10 +0200

    Bugfix: 3963 Writing http://... in VisualEditor doesn't auto-make it a link

--- Website/Composite/content/misc/editors/visualeditor/visualeditor.js
+++ Website/Composite/content/misc/editors/visualeditor/visualeditor.js
@@ -7,7 +7,7 @@ var config = {
 	elements : "editor",
 	theme : "composite",
 	browsers : "msie,gecko",
-	plugins: "compositelink,compositetable,compositeimage,compositerendering,compositecharmap,compositefield,compositetext,compositespellcheck,paste,lists",	
+	plugins: "autolink,compositelink,compositetable,compositeimage,compositerendering,compositecharmap,compositefield,compositetext,compositespellcheck,paste,lists",	
 	verify_html : false,
 	entity_encoding : "raw",
 	convert_fonts_to_spans : false,
