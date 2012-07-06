ae78a88d7b5df7e3f59420ef672033a78490a102
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2012-07-06 13:29:45 +0200

    Fixing 4000 "Composite.Utils.Predicates.StringInCommaSeparatedList in Get<DataType>Xml filters throws an exception"

--- Composite/Plugins/Functions/FunctionProviders/StandardFunctionProvider/Utils/Predicates/StringInCommaSeparatedListPredicateFunction.cs
+++ Composite/Plugins/Functions/FunctionProviders/StandardFunctionProvider/Utils/Predicates/StringInCommaSeparatedListPredicateFunction.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
-using System.Data.SqlTypes;
-
+using System.Reflection;
 using Composite.Functions;
 
 using Composite.Plugins.Functions.FunctionProviders.StandardFunctionProvider.Foundation;
@@ -13,6 +11,13 @@ namespace Composite.Plugins.Functions.FunctionProviders.StandardFunctionProvider
 {
     internal sealed class StringInCommaSeparatedListPredicateFunction : StandardFunctionBase
     {
+        private static readonly MethodInfo _stringCompareMethodInfo;
+
+        static StringInCommaSeparatedListPredicateFunction()
+        {
+            _stringCompareMethodInfo = typeof(string).GetMethod("Compare", new[] { typeof(string), typeof(string), typeof(StringComparison) });
+        }
+
         public StringInCommaSeparatedListPredicateFunction(EntityTokenFactory entityTokenFactory)
             : base("StringInCommaSeparatedList", "Composite.Utils.Predicates", typeof(Expression<Func<string, bool>>), entityTokenFactory)
         {
@@ -36,29 +41,50 @@ namespace Composite.Plugins.Functions.FunctionProviders.StandardFunctionProvider
 
         public override object Execute(ParameterList parameters, FunctionContextContainer context)
         {
-            Expression<Func<string, bool>> predicate = f => false;
+            Expression<Func<string, bool>> emptyPredicate = f => false;
 
             string commaSeparatedSearchTerms = parameters.GetParameter<string>("CommaSeparatedSearchTerms");
 
-            if (string.IsNullOrEmpty(commaSeparatedSearchTerms)==true)
+            if (commaSeparatedSearchTerms.IsNullOrEmpty())
             {
-                return predicate;
+                return emptyPredicate;
             }
 
+
             bool ignoreCase = parameters.GetParameter<bool>("IgnoreCase");
-            IEnumerable<string> searchTerms = commaSeparatedSearchTerms.Split(',');
+            string[] searchTerms = commaSeparatedSearchTerms.Split(new [] {','}/*, StringSplitOptions.RemoveEmptyEntries*/);
+
+            if (searchTerms.Length == 0)
+            {
+                return emptyPredicate;
+            }
 
             StringComparison stringComparison = (ignoreCase
-                                                     ? StringComparison.CurrentCultureIgnoreCase
-                                                     : StringComparison.CurrentCulture);
+                                                     ? StringComparison.OrdinalIgnoreCase
+                                                     : StringComparison.Ordinal);
+
+
+            var parameterExpression = Expression.Parameter(typeof(string), "p");
+            Expression body = null;
 
             foreach (string searchTerm in searchTerms)
             {
-                string temp = searchTerm.Trim();
-                predicate = predicate.Or(c => c.Equals(temp, stringComparison));
-            }
+                string termTrimmed = searchTerm.Trim();
 
-            return predicate;
+                // string.Compare(p, termTrimmed, stringComparison) == 0 
+                // string.Compare() is supported by linq2sql
+                Expression condition = Expression.Equal(Expression.Call(
+                                                           _stringCompareMethodInfo,
+                                                           parameterExpression,
+                                                           Expression.Constant(termTrimmed),
+                                                           Expression.Constant(stringComparison)), 
+                                                       Expression.Constant(0));
+
+                body = body == null ? condition : Expression.Or(body, condition);
+            }
+            Verify.IsNotNull(body, "Expression body is null");
+
+            return Expression.Lambda<Func<string, bool>>(body, parameterExpression);
         }
     }
 }
