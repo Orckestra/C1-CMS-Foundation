15397bbd0596e47438bfd601c127c1f97d0accfd
Marcus Wendt <marcus@wendt.dk>
2012-08-30 13:00:47 +0200

    Fixing 4048 - workflows could get serialized/deserialized using different Cultures. Now always use CultureInvariant

--- Composite/C1Console/Workflow/Foundation/FormData.cs
+++ Composite/C1Console/Workflow/Foundation/FormData.cs
@@ -11,6 +11,8 @@ using Composite.Core.Serialization;
 using Composite.Core.Types;
 using Composite.Data.Validation.ClientValidationRules;
 using Composite.Core.Xml;
+using Composite.Core.Threading;
+using System.Globalization;
 
 
 namespace Composite.C1Console.Workflow.Foundation
@@ -56,7 +58,9 @@ namespace Composite.C1Console.Workflow.Foundation
         /// <exclude />
         public XElement Serialize()
         {
-            IXmlSerializer xmlSerializer = new XmlSerializer(new IValueXmlSerializer[] {
+            using (var cultureInvariant = new ThreadCultureScope(CultureInfo.InvariantCulture))
+            {
+                IXmlSerializer xmlSerializer = new XmlSerializer(new IValueXmlSerializer[] {
                 new SystemPrimitivValueXmlSerializer(),
                 new SystemCollectionValueXmlSerializer(),
                 new SystemTypesValueXmlSerializer(),
@@ -68,79 +72,80 @@ namespace Composite.C1Console.Workflow.Foundation
                 new SystemSerializableValueXmlSerializer() });
 
 
-            XElement containerLabelElement = new XElement("ContainerLabel");
-            if (this.ContainerLabel != null)
-            {
-                containerLabelElement.Add(new XAttribute("value", this.ContainerLabel));
-            }
-
-
-            XElement formDefinitionElement = new XElement("FormDefinition");
-            if (this.FormDefinition != null)
-            {
-                formDefinitionElement.Add(new XAttribute("value", this.FormDefinition));
-            }
-            else
-            {
-                using (XmlReader xmlReader = this.FormMarkupProvider.GetReader())
+                XElement containerLabelElement = new XElement("ContainerLabel");
+                if (this.ContainerLabel != null)
                 {
-                    xmlReader.MoveToContent();
-                    XElement element = (XElement)XDocument.ReadFrom(xmlReader);
-                    XDocument document = new XDocument(element);
-                    string content = document.GetDocumentAsString();
-                    formDefinitionElement.Add(new XAttribute("value", content));
+                    containerLabelElement.Add(new XAttribute("value", this.ContainerLabel));
                 }
-            }
 
 
-            XElement customToolbarDefinitionElement = new XElement("CustomToolbarDefinition");
-            if (this.CustomToolbarDefinition != null)
-            {
-                customToolbarDefinitionElement.Add(new XAttribute("value", this.CustomToolbarDefinition));
-            }
-            else if (this.CustomToolbarMarkupProvider != null)
-            {
-                using (XmlReader xmlReader = this.CustomToolbarMarkupProvider.GetReader())
+                XElement formDefinitionElement = new XElement("FormDefinition");
+                if (this.FormDefinition != null)
                 {
-                    xmlReader.MoveToContent();
-                    XElement element = (XElement)XDocument.ReadFrom(xmlReader);
-                    XDocument document = new XDocument(element);
-                    string content = document.GetDocumentAsString();
-                    customToolbarDefinitionElement.Add(new XAttribute("value", content));
+                    formDefinitionElement.Add(new XAttribute("value", this.FormDefinition));
                 }
+                else
+                {
+                    using (XmlReader xmlReader = this.FormMarkupProvider.GetReader())
+                    {
+                        xmlReader.MoveToContent();
+                        XElement element = (XElement)XDocument.ReadFrom(xmlReader);
+                        XDocument document = new XDocument(element);
+                        string content = document.GetDocumentAsString();
+                        formDefinitionElement.Add(new XAttribute("value", content));
+                    }
+                }
+
+
+                XElement customToolbarDefinitionElement = new XElement("CustomToolbarDefinition");
+                if (this.CustomToolbarDefinition != null)
+                {
+                    customToolbarDefinitionElement.Add(new XAttribute("value", this.CustomToolbarDefinition));
+                }
+                else if (this.CustomToolbarMarkupProvider != null)
+                {
+                    using (XmlReader xmlReader = this.CustomToolbarMarkupProvider.GetReader())
+                    {
+                        xmlReader.MoveToContent();
+                        XElement element = (XElement)XDocument.ReadFrom(xmlReader);
+                        XDocument document = new XDocument(element);
+                        string content = document.GetDocumentAsString();
+                        customToolbarDefinitionElement.Add(new XAttribute("value", content));
+                    }
+                }
+
+
+                XElement containerTypeElement = xmlSerializer.Serialize(typeof(IFlowUiContainerType), this.ContainerType);
+
+
+                Dictionary<string, object> selectedBindings =
+                    (from kvp in this.Bindings
+                     where (kvp.Value != null &&
+                            kvp.Value.GetType() != typeof(System.EventHandler)) ||
+                            kvp.Value == null
+                     select kvp).ToDictionary(f => f.Key, f => f.Value);
+                XElement bindingsElement = xmlSerializer.Serialize(typeof(Dictionary<string, object>), selectedBindings);
+
+                XElement bindingsValidationRulesElement = xmlSerializer.Serialize(typeof(Dictionary<string, List<ClientValidationRule>>), this.BindingsValidationRules);
+
+                XElement excludedEventsElement = xmlSerializer.Serialize(typeof(List<string>), ExcludedEvents);
+
+                XElement formDataElement = new XElement("FormData");
+                formDataElement.Add(containerLabelElement);
+                formDataElement.Add(formDefinitionElement);
+                formDataElement.Add(customToolbarDefinitionElement);
+                formDataElement.Add(new XElement("ContainerType", containerTypeElement));
+                formDataElement.Add(new XElement("Bindings", bindingsElement));
+                formDataElement.Add(new XElement("BindingsValidationRules", bindingsValidationRulesElement));
+                if (excludedEventsElement != null) formDataElement.Add(new XElement("ExcludedEvents", excludedEventsElement));
+
+                if (this.EventHandleFilterType != null)
+                {
+                    formDataElement.Add(new XElement("EventHandleFilterType", new XAttribute("type", TypeManager.SerializeType(this.EventHandleFilterType))));
+                }
+
+                return formDataElement;
             }
-
-
-            XElement containerTypeElement = xmlSerializer.Serialize(typeof(IFlowUiContainerType), this.ContainerType);
-
-
-            Dictionary<string, object> selectedBindings =
-                (from kvp in this.Bindings
-                 where (kvp.Value != null &&
-                        kvp.Value.GetType() != typeof(System.EventHandler)) ||
-                        kvp.Value == null
-                 select kvp).ToDictionary(f => f.Key, f => f.Value);
-            XElement bindingsElement = xmlSerializer.Serialize(typeof(Dictionary<string, object>), selectedBindings);
-
-            XElement bindingsValidationRulesElement = xmlSerializer.Serialize(typeof(Dictionary<string, List<ClientValidationRule>>), this.BindingsValidationRules);
-
-            XElement excludedEventsElement = xmlSerializer.Serialize(typeof(List<string>), ExcludedEvents);           
-
-            XElement formDataElement = new XElement("FormData");
-            formDataElement.Add(containerLabelElement);
-            formDataElement.Add(formDefinitionElement);
-            formDataElement.Add(customToolbarDefinitionElement);
-            formDataElement.Add(new XElement("ContainerType", containerTypeElement));
-            formDataElement.Add(new XElement("Bindings", bindingsElement));
-            formDataElement.Add(new XElement("BindingsValidationRules", bindingsValidationRulesElement));
-            if (excludedEventsElement != null) formDataElement.Add(new XElement("ExcludedEvents", excludedEventsElement));
-
-            if (this.EventHandleFilterType != null)
-            {
-                formDataElement.Add(new XElement("EventHandleFilterType", new XAttribute("type", TypeManager.SerializeType(this.EventHandleFilterType))));
-            }
-
-            return formDataElement;
         }
 
 
@@ -148,7 +153,9 @@ namespace Composite.C1Console.Workflow.Foundation
         /// <exclude />
         public static FormData Deserialize(XElement serializedData)
         {
-            IXmlSerializer xmlSerializer = new XmlSerializer(new IValueXmlSerializer[] {
+            using (var cultureInvariant = new ThreadCultureScope(CultureInfo.InvariantCulture))
+            {
+                IXmlSerializer xmlSerializer = new XmlSerializer(new IValueXmlSerializer[] {
                 new SystemPrimitivValueXmlSerializer(),
                 new SystemCollectionValueXmlSerializer(),
                 new SystemTypesValueXmlSerializer(),
@@ -160,58 +167,59 @@ namespace Composite.C1Console.Workflow.Foundation
                 new SystemSerializableValueXmlSerializer()});
 
 
-            FormData formData = new FormData();
+                FormData formData = new FormData();
 
-            XElement containerLabelElement = serializedData.Elements("ContainerLabel").Single();
-            XAttribute containerLabelValueAttribute = containerLabelElement.Attribute("value");
-            if (containerLabelValueAttribute != null)
-            {
-                formData.ContainerLabel = containerLabelValueAttribute.Value;
+                XElement containerLabelElement = serializedData.Elements("ContainerLabel").Single();
+                XAttribute containerLabelValueAttribute = containerLabelElement.Attribute("value");
+                if (containerLabelValueAttribute != null)
+                {
+                    formData.ContainerLabel = containerLabelValueAttribute.Value;
+                }
+
+                XElement formDefinitionElement = serializedData.Elements("FormDefinition").Single();
+                XAttribute formDefinitionValueAttribute = formDefinitionElement.Attribute("value");
+                if (formDefinitionValueAttribute != null)
+                {
+                    formData.FormDefinition = formDefinitionValueAttribute.Value;
+                    formData.FormMarkupProvider = new StringBasedFormMarkupProvider(formDefinitionValueAttribute.Value);
+                }
+
+                XElement customToolbarDefinitionElement = serializedData.Elements("CustomToolbarDefinition").Single();
+                XAttribute customToolbarDefinitionValueAttribute = customToolbarDefinitionElement.Attribute("value");
+                if (customToolbarDefinitionValueAttribute != null)
+                {
+                    formData.CustomToolbarDefinition = customToolbarDefinitionValueAttribute.Value;
+                    formData.CustomToolbarMarkupProvider = new StringBasedFormMarkupProvider(customToolbarDefinitionValueAttribute.Value);
+                }
+
+                XElement containerTypeElement = serializedData.Elements("ContainerType").Single();
+                object containerType = xmlSerializer.Deserialize(containerTypeElement.Elements().Single());
+                formData.ContainerType = (IFlowUiContainerType)containerType;
+
+                XElement bindingsElement = serializedData.Elements("Bindings").Single();
+                object bindings = xmlSerializer.Deserialize(bindingsElement.Elements().Single());
+                formData.Bindings = (Dictionary<string, object>)bindings;
+
+                XElement bindingsValidationRulesElement = serializedData.Elements("BindingsValidationRules").Single();
+                object bindingsValidationRules = xmlSerializer.Deserialize(bindingsValidationRulesElement.Elements().Single());
+                formData.BindingsValidationRules = (Dictionary<string, List<ClientValidationRule>>)bindingsValidationRules;
+
+                XElement eventHandleFilterTypeElement = serializedData.Elements("EventHandleFilterType").SingleOrDefault();
+                if (eventHandleFilterTypeElement != null)
+                {
+                    Type eventHandleFilterType = TypeManager.GetType(eventHandleFilterTypeElement.Attribute("type").Value);
+                    formData.EventHandleFilterType = eventHandleFilterType;
+                }
+
+                XElement excludedEventsElement = serializedData.Elements("ExcludedEvents").SingleOrDefault();
+                if (excludedEventsElement != null)
+                {
+                    object excludedEvents = xmlSerializer.Deserialize(excludedEventsElement.Elements().Single());
+                    formData.ExcludedEvents = (List<string>)excludedEvents;
+                }
+
+                return formData;
             }
-
-            XElement formDefinitionElement = serializedData.Elements("FormDefinition").Single();
-            XAttribute formDefinitionValueAttribute = formDefinitionElement.Attribute("value");
-            if (formDefinitionValueAttribute != null)
-            {
-                formData.FormDefinition = formDefinitionValueAttribute.Value;
-                formData.FormMarkupProvider = new StringBasedFormMarkupProvider(formDefinitionValueAttribute.Value);
-            }
-
-            XElement customToolbarDefinitionElement = serializedData.Elements("CustomToolbarDefinition").Single();
-            XAttribute customToolbarDefinitionValueAttribute = customToolbarDefinitionElement.Attribute("value");
-            if (customToolbarDefinitionValueAttribute != null)
-            {
-                formData.CustomToolbarDefinition = customToolbarDefinitionValueAttribute.Value;
-                formData.CustomToolbarMarkupProvider = new StringBasedFormMarkupProvider(customToolbarDefinitionValueAttribute.Value);
-            }
-
-            XElement containerTypeElement = serializedData.Elements("ContainerType").Single();
-            object containerType = xmlSerializer.Deserialize(containerTypeElement.Elements().Single());
-            formData.ContainerType = (IFlowUiContainerType)containerType;
-                        
-            XElement bindingsElement = serializedData.Elements("Bindings").Single();
-            object bindings = xmlSerializer.Deserialize(bindingsElement.Elements().Single());
-            formData.Bindings = (Dictionary<string, object>)bindings;
-
-            XElement bindingsValidationRulesElement = serializedData.Elements("BindingsValidationRules").Single();
-            object bindingsValidationRules = xmlSerializer.Deserialize(bindingsValidationRulesElement.Elements().Single());
-            formData.BindingsValidationRules = (Dictionary<string, List<ClientValidationRule>>)bindingsValidationRules;
-
-            XElement eventHandleFilterTypeElement = serializedData.Elements("EventHandleFilterType").SingleOrDefault();
-            if (eventHandleFilterTypeElement != null)
-            {
-                Type eventHandleFilterType = TypeManager.GetType(eventHandleFilterTypeElement.Attribute("type").Value);
-                formData.EventHandleFilterType = eventHandleFilterType;
-            }
-
-            XElement excludedEventsElement = serializedData.Elements("ExcludedEvents").SingleOrDefault();
-            if (excludedEventsElement != null)
-            {
-                object excludedEvents = xmlSerializer.Deserialize(excludedEventsElement.Elements().Single());
-                formData.ExcludedEvents = (List<string>)excludedEvents;
-            }
-
-            return formData;
         }
     }
 }
---
+++ Composite/Core/Threading/ThreadCultureScope.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Threading;
+
+namespace Composite.Core.Threading
+{
+    /// <summary>
+    /// Will set the threads Culture and reset it to the original value when this is disposed of.
+    /// <example>
+    /// using( var cultureScope = new ThreadCultureScope( new CultureInfo("da-DK") ) )
+    /// {
+    ///   // Code here will run in da-DK scope. 
+    ///   // Culture in effect before the using statement will be reset when exiting the using.
+    /// }
+    /// </example>
+    /// </summary>
+    public sealed class ThreadCultureScope : IDisposable
+    {
+        private CultureInfo _originalCulture = null;
+        private CultureInfo _originalUiCulture = null;
+        private CultureInfo _desiredCulture = null;
+        private CultureInfo _desiredUiCulture = null;
+        private bool _disposed = false;
+
+        /// <summary>
+        /// Constructs a new culture scope, setting the threads CurrentCulture and resetting when this is disposed. The CurrentUiCulture is not affected.
+        /// </summary>
+        /// <param name="culture">Desired culture to be in effect</param>
+        public ThreadCultureScope(CultureInfo culture)
+        {
+            _originalCulture = Thread.CurrentThread.CurrentCulture;
+            _desiredCulture = culture;
+
+            Thread.CurrentThread.CurrentCulture = culture;
+        }
+
+
+        /// <summary>
+        /// Constructs a new culture scope, setting the threads CurrentCulture and CurrentUiCulture and resetting when this is disposed.
+        /// </summary>
+        /// <param name="culture">Desired culture to be in effect</param>
+        /// <param name="uiCulture">Desired UI culture to be in effect</param>
+        public ThreadCultureScope(CultureInfo culture, CultureInfo uiCulture)
+            : this(culture)
+        {
+            _originalUiCulture = Thread.CurrentThread.CurrentUICulture;
+            _desiredUiCulture = uiCulture;
+
+            Thread.CurrentThread.CurrentUICulture = uiCulture;
+        }
+
+
+        /// <summary>
+        /// The culture this class was constructed to use
+        /// </summary>
+        public CultureInfo Culture
+        {
+            get { return _desiredCulture; }
+        }
+
+
+        /// <summary>
+        /// The UI culture this class was constructed to use or null if none were specified.
+        /// </summary>
+        public CultureInfo UiCulture
+        {
+            get { return _desiredUiCulture; }
+        }
+
+        
+        /// <summary>
+        /// Return thread culture settings to their original values.
+        /// </summary>
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                if (_originalCulture!=null)
+                    Thread.CurrentThread.CurrentCulture = _originalCulture;
+
+                if (_originalUiCulture != null)
+                    Thread.CurrentThread.CurrentUICulture = _originalUiCulture;
+
+                _disposed = true;                
+            }
+        }
+    }
+}
