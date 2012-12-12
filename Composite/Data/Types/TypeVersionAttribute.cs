0c22551c58c213723cca1cbe14da548049c4e561
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2012-12-12 13:41:25 +0100

    Reintroducing TypeVersionAttribute for compatibiilty with the old Extranet package

---
+++ Composite/Data/Types/TypeVersionAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Composite.Data
+{
+    /// <exclude />
+    [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+    [Obsolete("This is no longer used in C1. From version 3.0", true)]
+    public sealed class TypeVersionAttribute : Attribute
+    {
+        /// <exclude />
+        public TypeVersionAttribute(int version)
+        {
+            this.Version = version;
+        }
+
+        /// <exclude />
+        public int Version { get; private set; }
+    }
+}
