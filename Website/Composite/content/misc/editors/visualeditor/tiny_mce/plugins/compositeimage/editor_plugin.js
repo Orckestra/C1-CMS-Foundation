1a6500fd58b4469900746a99ffae609ecb0af2d1
Taras Nakonechnyi <neexite@gmail.com>
2013-02-05 11:42:26 +0100

    Bugfix  [4.0] Image Properties not launching on double click

--- Website/Composite/content/misc/editors/visualeditor/tiny_mce/plugins/compositeimage/editor_plugin.js
+++ Website/Composite/content/misc/editors/visualeditor/tiny_mce/plugins/compositeimage/editor_plugin.js
@@ -39,21 +39,14 @@ new function () {
 
 			this.editor = ed;
 
-			/*
-			* Doubleclick feature enabled for Mozilla only. 
-			* IE looses the selection-bookmark when IMG gets  
-			* doubleclicked immediately after closing the dialog.
-			*/
-			if (Client.isMozilla) {
-				var self = this;
-				ed.onDblClick.add(function (editor, e) {
-					if (VisualEditorBinding.isImageElement(e.target)) {
-						self._img = e.target;
-						self.execCommand("compositeInsertImage", true, "update");
-						self._img = null;
-					}
-				});
-			}
+			var self = this;
+			ed.onDblClick.add(function (editor, e) {
+				if (VisualEditorBinding.isImageElement(e.target)) {
+					self._img = e.target;
+					self.execCommand("compositeInsertImage", true, "update");
+					self._img = null;
+				}
+			});
 		},
 
 		/**
