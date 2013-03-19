1614c8c7ee7824d7b9aab8bcc48212800ca83988
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2013-03-19 13:11:11 +0100

    Rolling back changeset 13161

---
+++ Website/Composite/controls/FormsControls/Helpers/StyleLoaderControl.ascx.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Web.UI;
+
+namespace Composite.controls.helpers
+{
+    public partial class StyleLoaderControl : System.Web.UI.UserControl
+    {
+        public string adminRelativePath = "xxx";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string path = Composite.Core.WebClient.UrlUtils.ResolveAdminUrl(adminRelativePath);
+            string tagGoo = string.Format("<link rel='stylesheet' type='text/css' href='{0}'/>\n", path);
+
+            Control headerElementsPlaceHolder = this.FindControl(this.Page, "HeaderPlaceHolder");
+            if (headerElementsPlaceHolder == null) throw new InvalidOperationException("Missing 'HeaderPlaceHolder' placeholder on page");
+            headerElementsPlaceHolder.Controls.Add(new LiteralControl(tagGoo));
+        }
+
+
+        private Control FindControl(Control toSearch, string idToFind)
+        {
+            Control c = toSearch.FindControl(idToFind);
+            if (c == null && toSearch.Controls != null)
+            {
+                foreach (Control subControl in toSearch.Controls)
+                {
+                    c = FindControl(subControl, idToFind);
+                    if (c != null) break;
+                }
+            }
+
+            return c;
+        }
+
+
+    }
+}
