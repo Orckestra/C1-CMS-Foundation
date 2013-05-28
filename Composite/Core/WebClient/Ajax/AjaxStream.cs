7f7f2291419c441142ecaa4d1777e007bba41686
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2013-05-28 16:28:23 +0200

    Fixing 4410 OutOfMemoryException when downloading large backup zips

--- Composite/Core/WebClient/Ajax/AjaxStream.cs
+++ Composite/Core/WebClient/Ajax/AjaxStream.cs
@@ -1,129 +1,38 @@
 ﻿using System;
 using System.IO;
-using System.Text;
 using System.Web;
+using Composite.Core.WebClient.HttpModules;
 
 
 namespace Composite.Core.WebClient.Ajax
 {
-	internal class AjaxStream: Stream
-	{
+    internal class AjaxStream : Utf8StringTransformationStream
+    {
         private static readonly string ScriptManagerJS = "UpdateManager.xhtml = null;";
 
-	    private readonly Stream _innerStream;
-        private MemoryStream _ms = new MemoryStream();
+        public AjaxStream(Stream innerOuputStream): base(innerOuputStream) { }
 
-	    public AjaxStream(Stream innerOuputStream)
-	    {
-	        _innerStream = innerOuputStream;
-	    }
-
-        public override bool CanRead
+        public override string Process(string str)
         {
-            get { return false; }
-        }
-
-        public override bool CanSeek
-        {
-            get { return false; }
-        }
-
-        public override bool CanWrite
-        {
-            get { return true; }
-        }
-
-        public override void Flush()
-        {
-            // DO NOTHING HERE
-        }
-
-        public override long Length
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public override long Position
-        {
-            get
+            int jsCodePosition = str.IndexOf(ScriptManagerJS, StringComparison.Ordinal);
+            if (jsCodePosition == -1)
             {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public override int Read(byte[] buffer, int offset, int count)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override long Seek(long offset, SeekOrigin origin)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void SetLength(long value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void Write(byte[] buffer, int offset, int count)
-        {
-            if(!_ms.CanWrite)
-            {
-                // Reopening stream if it was empty
-                _ms = new MemoryStream();
-            }
-            _ms.Write(buffer, offset, count);
-        }
-
-        public override void Close()
-        {
-            // Checking if the stream was already closed
-            if(!_ms.CanSeek)
-            {
-                return;
+                return str;
             }
 
-            _ms.Seek(0, SeekOrigin.Begin);
-
-            var bytes = _ms.ToArray();
-
-            string str = Encoding.UTF8.GetString(bytes);
-
-            bool changed = false;
-
-            int jsCodePosition = str.IndexOf(ScriptManagerJS);
-            if (jsCodePosition > -1)
+            // Removing encoding symbol
+            if (str[0] != '<') // Method StartsWith(...) doesn't work correctly here
             {
-                // Removing encoding symbol
-                if (str[0] != '<') // Method StartsWith(...) doesn't work correctly here
-                {
-                    str = str.Substring(1);
-                    jsCodePosition--;
-                }
-
-                string encodedText = HttpContext.Current.Server.UrlEncode(str).Replace("+", "%20"); //EncodeJsString(str);
-
-                str = string.Format("{0}UpdateManager.xhtml = \"{1}\";{2}",
-                                    str.Substring(0, jsCodePosition),
-                                    encodedText,
-                                    str.Substring(jsCodePosition + ScriptManagerJS.Length));
-                changed = true;
+                str = str.Substring(1);
+                jsCodePosition--;
             }
 
-            if (changed)
-            {
-                bytes = Encoding.UTF8.GetBytes(str);
-            }
+            string encodedText = HttpContext.Current.Server.UrlEncode(str).Replace("+", "%20"); //EncodeJsString(str);
 
-            _innerStream.Write(bytes, 0, bytes.Length);
-
-            _innerStream.Close();
-            _ms.Close();
+            return string.Format("{0}UpdateManager.xhtml = \"{1}\";{2}",
+                                 str.Substring(0, jsCodePosition),
+                                 encodedText,
+                                 str.Substring(jsCodePosition + ScriptManagerJS.Length));
         }
-	}
+    }
 }
