5f980c260b40274d3b910033abdedc6a7aa98524
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2013-06-03 16:25:45 +0200

    Fixing unnecessary redirect to /Composite/Login.aspx?=.. on preview, which also fixed an issue with Azure deployment

--- Composite/Core/WebClient/Renderings/Page/PagePreviewBuilder.cs
+++ Composite/Core/WebClient/Renderings/Page/PagePreviewBuilder.cs
@@ -42,6 +42,8 @@ namespace Composite.Core.WebClient.Renderings.Page
                 // asp.net 4.5 request validation will see the 'page edit http post' data and start bitching. It really should not.
                 var headers = new System.Collections.Specialized.NameValueCollection();
                 headers.Add("Content-Length", "0");
+                headers.Add("Cookie", ctx.Request.Headers["Cookie"]);
+
                 ctx.Server.TransferRequest("~/Renderers/Page.aspx?" + query, false, "GET", headers);
             }
             else
