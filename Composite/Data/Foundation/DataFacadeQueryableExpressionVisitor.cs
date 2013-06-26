ebc9942f161398c12e68ea2389a10fbcd2b61bd5
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2013-06-26 12:20:22 +0200

    Optimizing multiple source LINQ queries

--- Composite/Data/Foundation/DataFacadeQueryableExpressionVisitor.cs
+++ Composite/Data/Foundation/DataFacadeQueryableExpressionVisitor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Data.Linq;
 using System.Linq;
 using System.Linq.Expressions;
@@ -11,6 +12,9 @@ using Composite.Plugins.Data.DataProviders.MSSqlServerDataProvider.CodeGeneratio
 
 namespace Composite.Data.Foundation
 {
+    /// <summary>
+    /// Pulls queries into the memory if needed. Handles multiple source IQueryable-s
+    /// </summary>
     internal sealed class DataFacadeQueryableExpressionVisitor : ExpressionVisitor
     {
         private static readonly MethodInfo _dataFacadeGetDataMethodInfo = typeof(DataFacade).GetMethods().First(x => x.Name == "GetData" && x.IsGenericMethod);
@@ -18,6 +22,9 @@ namespace Composite.Data.Foundation
 
         private static readonly MethodInfo Queryable_Where = StaticReflection.GetGenericMethodInfo(() => System.Linq.Queryable.Where(null, (Expression<Func<int, bool>>)null));
         private static readonly MethodInfo Queryable_Any = typeof(Queryable).GetMethods().Single(x => x.Name == "Any" && x.IsGenericMethod && x.GetParameters().Count() == 1);
+        private static readonly MethodInfo Queryable_Count = typeof(Queryable).GetMethods().Single(x => x.Name == "Count" && x.IsGenericMethod && x.GetParameters().Count() == 1);
+        private static readonly MethodInfo Queryable_FirstOrDefault = typeof(Queryable).GetMethods().Single(x => x.Name == "FirstOrDefault" && x.IsGenericMethod && x.GetParameters().Count() == 1);
+        private static readonly MethodInfo Queryable_Take = typeof(Queryable).GetMethods().Single(x => x.Name == "Take" && x.IsGenericMethod && x.GetParameters().Count() == 2);
 
         private readonly bool _pullAllToMemory;
         private IQueryable _queryable;
@@ -103,7 +110,7 @@ namespace Composite.Data.Foundation
                     return Expression.Constant(result is IDataFacadeQueryable ? HandleMultipleSourceQueryable(result) : result);
                 }
             
-                throw new NotImplementedException("Supporing for DataFacade method '{0}' or one of it's overloads not yet implemented".FormatWith(m.Method.Name));
+                throw new NotSupportedException("Supporing for DataFacade method '{0}' or one of it's overloads not yet implemented".FormatWith(m.Method.Name));
             }
 
             if (m.Method.DeclaringType == typeof (DataConnection))
@@ -117,7 +124,7 @@ namespace Composite.Data.Foundation
                     return Expression.Constant(result is IDataFacadeQueryable ? HandleMultipleSourceQueryable(result) : result);
                 }
 
-                throw new NotImplementedException("Supporing for DataConnection method '{0}' or one of it's overloads not yet implemented".FormatWith(m.Method.Name));
+                throw new NotSupportedException("Supporing for DataConnection method '{0}' or one of it's overloads not yet implemented".FormatWith(m.Method.Name));
             }
 
             // Replacing Guid.NewGuid() call with "newid()" sql statement
@@ -132,42 +139,137 @@ namespace Composite.Data.Foundation
             }
 
             // Processing queries that have multiple IQueryable sources 
+
+            // Processing queries like
+            //
+            // multipleSourceQueryable.METHOD()
+            // multipleSourceQueryable.METHOD(predicate)
+            // multipleSourceQueryable.Where(predicate).METHOD()
+            // multipleSourceQueryable.Where(predicate1).METHOD(predicate2)
+            //
+            // Where the supported METHOD options are: "Where", "Any", "Count", "First" and "FirstOrDefault"
+
             if (m.Method.IsStatic
-                && (m.Method.Name == "Where" || m.Method.Name == "FirstOrDefault" || m.Method.Name == "Any" || m.Method.Name == "First")
-                && m.Arguments.Count == 2
-                && m.Arguments[1] is UnaryExpression
-                && m.Arguments[0] is ConstantExpression
-                && (m.Arguments[0] as ConstantExpression).Value is IDataFacadeQueryable
-                && ((m.Arguments[0] as ConstantExpression).Value as IDataFacadeQueryable).Sources.Count() > 1)
+                && (m.Method.Name == "Where"
+                    || m.Method.Name == "Any"
+                    || m.Method.Name == "Count"
+                    || m.Method.Name == "FirstOrDefault" 
+                    || m.Method.Name == "First")
+                && ( IsMultipleSourceQueryable(m.Arguments[0])
+                     || (m.Arguments[0] is MethodCallExpression
+                         && (m.Arguments[0] as MethodCallExpression).Method.Name == "Where"
+                         && IsMultipleSourceQueryable((m.Arguments[0] as MethodCallExpression).Arguments[0])
+                         && (m.Arguments[0] as MethodCallExpression).Arguments[1] is UnaryExpression))
+                && (m.Arguments.Count == 1
+                    || (m.Arguments.Count == 2 
+                        && m.Arguments[1] is UnaryExpression)))
             {
-                var condition = m.Arguments[1] as UnaryExpression;
-                var sources = ((m.Arguments[0] as ConstantExpression).Value as IDataFacadeQueryable).Sources.ToArray();
+                IQueryable[] sources;
+                Expression predicate1;
+
+                if (IsMultipleSourceQueryable(m.Arguments[0]))
+                {
+                    sources = GetSourceQueries(m.Arguments[0]);
+                    predicate1 = null;
+                }
+                else
+                {
+                    var whereExpression = m.Arguments[0] as MethodCallExpression;
+
+                    sources = GetSourceQueries(whereExpression.Arguments[0]);
+                    predicate1 = (whereExpression.Arguments[1] as UnaryExpression).Operand; 
+                }
+
+                Expression predicate2 = m.Arguments.Count == 2 ? (m.Arguments[1] as UnaryExpression).Operand : null;
+
+                var predicates = new List<Expression>();
+
+                if (predicate1 != null) predicates.Add(predicate1);
+                if (predicate2 != null) predicates.Add(predicate2);
 
                 if (m.Method.Name == "Any")
                 {
                     _queryable = _queryable ?? new bool[0].AsQueryable(); // TODO: refactor ?
 
-                    return Expression.Constant(Any(sources, condition.Operand));
+                    return Expression.Constant(Any(sources, predicates));
                 }
 
-                IQueryable loadedSet = LoadToMemory(sources, condition.Operand);
-
-                if (_queryable == null)
+                if (m.Method.Name == "Count")
                 {
-                    _queryable = loadedSet;
+                    _queryable = _queryable ?? new bool[0].AsQueryable();
+
+                    return Expression.Constant(Count(sources, predicates));
+                }
+
+                if (m.Method.Name == "FirstOrDefault")
+                {
+                    _queryable = _queryable ?? new bool[0].AsQueryable();
+
+                    return Expression.Constant(FirstOrDefault(sources, predicates));
+                }
+
+                if (m.Method.Name == "First")
+                {
+                    _queryable = _queryable ?? new bool[0].AsQueryable();
+
+                    return Expression.Constant(First(sources, predicates));
                 }
 
                 if (m.Method.Name == "Where")
                 {
+                    IQueryable loadedSet = LoadToMemory(sources, predicates);
+
+                    _queryable = _queryable ?? loadedSet;
+
                     return Expression.Constant(loadedSet);
                 }
 
-                var methodToCall = typeof (Queryable).GetMethods()
-                    .First(method => method.Name == m.Method.Name 
-                        && method.GetParameters().Length == 1)
-                    .MakeGenericMethod(m.Method.GetGenericArguments());
+                throw new InvalidOperationException("This code should not be reachable. Current expression: " + m);
+            }
 
-                return Expression.Call(methodToCall, Expression.Constant(loadedSet));
+            // Processing queries like
+            //
+            // multipleSourceQueryable.Take(N)
+            // multipleSourceQueryable.Where(predicate).Take(N)
+
+            if (m.Method.IsStatic
+                && (m.Method.Name == "Take")
+                && (IsMultipleSourceQueryable(m.Arguments[0])
+                     || (m.Arguments[0] is MethodCallExpression
+                         && (m.Arguments[0] as MethodCallExpression).Method.Name == "Where"
+                         && IsMultipleSourceQueryable((m.Arguments[0] as MethodCallExpression).Arguments[0])
+                         && (m.Arguments[0] as MethodCallExpression).Arguments[1] is UnaryExpression))
+                && (m.Arguments.Count == 2
+                    && m.Arguments[1] is ConstantExpression
+                    && (m.Arguments[1] as ConstantExpression).Value is int))
+            {
+                IQueryable[] sources;
+                Expression predicate;
+
+                int count = (int) (m.Arguments[1] as ConstantExpression).Value;
+
+                if (IsMultipleSourceQueryable(m.Arguments[0]))
+                {
+                    sources = GetSourceQueries(m.Arguments[0]);
+                    predicate = null;
+                }
+                else
+                {
+                    var whereExpression = m.Arguments[0] as MethodCallExpression;
+
+                    sources = GetSourceQueries(whereExpression.Arguments[0]);
+                    predicate = (whereExpression.Arguments[1] as UnaryExpression).Operand;
+                }
+
+                var predicates = new List<Expression>();
+
+                if (predicate != null) predicates.Add(predicate);
+
+                var result = Take(sources, count, predicates);
+
+                _queryable = _queryable ?? result;
+
+                return Expression.Constant(result);
             }
 
             return base.VisitMethodCall(m);
@@ -218,16 +320,13 @@ namespace Composite.Data.Foundation
             return elementType;
         }
 
-        private static bool Any(IQueryable[] sources, Expression predicate = null)
+        private static bool Any(IQueryable[] sources, IEnumerable<Expression> predicates = null)
         {
             Type elementType = GetElementType(sources);
 
             foreach (IQueryable query in sources)
             {
-                IQueryable filteredQuery = predicate != null
-                    ? (IQueryable)Queryable_Where.MakeGenericMethod(elementType).Invoke(null, new object[] { query, predicate })
-                    : query;
-
+                IQueryable filteredQuery = ApplyPredicates(query, elementType, predicates);
 
                 bool any = (bool) Queryable_Any.MakeGenericMethod(elementType).Invoke(null, new object[] { filteredQuery });
 
@@ -237,7 +336,94 @@ namespace Composite.Data.Foundation
             return false;
         }
 
-        private static IQueryable LoadToMemory(IQueryable[] sources, Expression predicate = null)
+        private static int Count(IQueryable[] sources, IEnumerable<Expression> predicates = null)
+        {
+            Type elementType = GetElementType(sources);
+
+            int result = 0;
+
+            foreach (IQueryable query in sources)
+            {
+                IQueryable filteredQuery = ApplyPredicates(query, elementType, predicates);
+
+                int count = (int) Queryable_Count.MakeGenericMethod(elementType).Invoke(null, new object[] { filteredQuery });
+
+                result += count;
+            }
+
+            return result;
+        }
+
+        private static object First(IQueryable[] sources, IEnumerable<Expression> predicates = null)
+        {
+            var result = FirstOrDefault(sources, predicates);
+
+            if (result == null)
+            {
+                string predicateInfo = predicates != null ? string.Join(" ANDALSO ", predicates.Select(p => p.ToString())) : "";
+                throw new InvalidOperationException("Sequence contains no elements. " + predicateInfo);
+            }
+
+            return result;
+        }
+
+        private static object FirstOrDefault(IQueryable[] sources, IEnumerable<Expression> predicates = null)
+        {
+            Type elementType = GetElementType(sources);
+
+
+            foreach (IQueryable query in sources)
+            {
+                IQueryable filteredQuery = ApplyPredicates(query, elementType, predicates);
+
+                object result = Queryable_FirstOrDefault.MakeGenericMethod(elementType).Invoke(null, new object[] { filteredQuery });
+
+                // Result can't be value type, so null is always the default value
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+
+            return null;
+        }
+
+
+        private static IQueryable Take(IQueryable[] sources, int count, IEnumerable<Expression> predicates = null)
+        {
+            Type elementType = GetElementType(sources);
+
+            MethodInfo addRangeToListMethodInfo = DataFacadeQueryableCache.GetAddRangeToListMethodInfo(elementType);
+
+            Type listType = typeof(List<>).MakeGenericType(elementType);
+            var List_Count = listType.GetProperty("Count");
+            var list = Activator.CreateInstance(listType);
+
+            int taken = 0;
+
+            foreach (IQueryable query in sources)
+            {
+                IQueryable filteredQuery = ApplyPredicates(query, elementType, predicates);
+
+                int elementsToTake = count - taken;
+
+                var subList = Queryable_Take.MakeGenericMethod(elementType).Invoke(null, new object[] { filteredQuery, elementsToTake });
+
+                addRangeToListMethodInfo.Invoke(list, new object[] { subList });
+
+                taken = (int) List_Count.GetValue(list, null);
+
+                if(taken == count) break;
+            }
+
+            MethodInfo asQueryableMethodInfo = DataFacadeQueryableCache.GetAsQueryableMethodInfo(elementType);
+
+            object listedDataAsQueryable = asQueryableMethodInfo.Invoke(null, new [] { list });
+
+            return (IQueryable)listedDataAsQueryable;
+        }
+
+        private static IQueryable LoadToMemory(IQueryable[] sources, IEnumerable<Expression> predicates = null)
         {
             Type elementType = GetElementType(sources);
 
@@ -249,10 +435,7 @@ namespace Composite.Data.Foundation
 
             foreach (IQueryable query in sources)
             {
-                IQueryable filteredQuery = predicate != null 
-                    ? (IQueryable)Queryable_Where.MakeGenericMethod(elementType).Invoke(null, new object[] { query, predicate })
-                    : query;
-
+                IQueryable filteredQuery = ApplyPredicates(query, elementType, predicates);
 
                 var subList = toListMethodInfo.Invoke(null, new object[] { filteredQuery });
 
@@ -264,6 +447,35 @@ namespace Composite.Data.Foundation
             object listedDataAsQueryable = asQueryableMethodInfo.Invoke(null, new object[] { listedData });
 
             return (IQueryable)listedDataAsQueryable;
+        }
+
+        private static IQueryable ApplyPredicates(IQueryable queryable, Type elementType,
+                                                  IEnumerable<Expression> predicates)
+        {
+            if (predicates != null)
+            {
+                foreach (var predicate in predicates)
+                {
+                    queryable = (IQueryable) Queryable_Where.MakeGenericMethod(elementType).Invoke(null, new object[] {queryable, predicate});
+                }
+            }
+
+            return queryable;
+        }
+
+
+        private static bool IsMultipleSourceQueryable(Expression expression)
+        {
+            var constantExpression = expression as ConstantExpression;
+
+            return constantExpression != null 
+                && constantExpression.Value is IDataFacadeQueryable
+                && (constantExpression.Value as IDataFacadeQueryable).Sources.Count() > 1;
+        }
+
+        private static IQueryable[] GetSourceQueries(Expression expression)
+        {
+            return  (((ConstantExpression) expression).Value as IDataFacadeQueryable).Sources.ToArray();
         }
 
         private IQueryable HandleMultipleSourceQueryable(object multipleSourceQueryableCandidate)
