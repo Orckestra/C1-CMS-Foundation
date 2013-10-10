0fc3a129b0d7d0a04bd6a18e1ad403c3e6039a84
Marcus Wendt <marcus@wendt.dk>
2013-10-10 22:27:47 +0200

    Making IImageFile available to API and intellisense

--- Composite/Data/Types/IImageFile.cs
+++ Composite/Data/Types/IImageFile.cs
@@ -4,9 +4,8 @@
 namespace Composite.Data.Types
 {
     /// <summary>    
+    /// This data interface represents a image media file in Composite C1. This can be used to query images through a <see cref="Composite.Data.DataConnection"/>. 
     /// </summary>
-    /// <exclude />
-    [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)] 
     [Title("C1 Image File")]
     [ImmutableTypeId("{BF54E59A-0EBC-4162-95B9-C46EE271C7A9}")]
     [KeyTemplatedXhtmlRenderer(XhtmlRenderingType.Embedable, XhtmlRenderingEncoding.AttributeContent, "<img src='~/media({field:StoreId}:{field:Id})' alt='{field:Title}' />")]
