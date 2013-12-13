b11a8992ebddcad499917432339e5273aa4b288a
Taras Nakonechnyi <neexite@gmail.com>
2013-12-13 11:44:28 +0100

    Css styling buttons without clean

--- Website/Composite/scripts/source/top/ui/bindings/buttons/ButtonBinding.js
+++ Website/Composite/scripts/source/top/ui/bindings/buttons/ButtonBinding.js
@@ -1,6 +1,6 @@
-ButtonBinding.prototype = new MatrixBinding;
+ButtonBinding.prototype = new Binding;
 ButtonBinding.prototype.constructor = ButtonBinding;
-ButtonBinding.superclass = MatrixBinding.prototype;
+ButtonBinding.superclass = Binding.prototype;
 
 ButtonBinding.ACTION_COMMAND = "buttoncommand";
 ButtonBinding.ACTION_RADIOBUTTON_ATTACHED = "radiobutton attached";
@@ -818,20 +818,7 @@ ButtonBinding.prototype.setEqualSizeWidth = function ( goal ) {
 ButtonBinding.prototype.getWidth = function () {
 	
 	var result = null;
-	if ( this.isAttached == true ) {
-	
-		var padding = CSSComputer.getPadding ( this.bindingElement );
-		var border = CSSComputer.getPadding ( this.bindingElement );
-	
-		result = this.shadowTree.c.offsetWidth + this.shadowTree.e.offsetWidth + this.shadowTree.w.offsetWidth;
-		result = result + padding.left + padding.right;
-		result = result + border.left + border.right;
-		
-	} else {
-		throw "ButtonBinding: getWidth failed for non-attached button."
-	}
-	return result;
-	
+	return this.bindingElement.offsetWidth;
 }
 
 /**
@@ -840,20 +827,11 @@ ButtonBinding.prototype.getWidth = function () {
  */
 ButtonBinding.prototype.setWidth = function ( width ) {
 	
-	if ( this.isAttached == true ) {
-		
-		var minus = this.shadowTree.e.offsetWidth + this.shadowTree.w.offsetWidth;
-		var padding = CSSComputer.getPadding ( this.shadowTree.c );
-		
-		var center = width - minus;
-		center = center - padding.left - padding.right;
-		this.shadowTree.c.style.width = String ( center ) + "px";
-		
-		if ( this.getProperty ( "centered" )) { // only relevant on splash for now...
-			this.labelBinding.bindingElement.style.marginLeft = String ( 0.5 * ( center - this.labelBinding.bindingElement.offsetWidth )) + "px";
-		}
+
+	if (width >= 0) {
+		this.bindingElement.style.width = new String(width + "px");
 	}
-	
+
 	this.setProperty ( "width", width );
 }
 
