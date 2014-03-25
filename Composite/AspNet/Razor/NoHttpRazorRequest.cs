f1b2fa84c4eec2b440df29ff0c39f3dce40cc934
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2014-03-25 16:10:23 +0100

    Fixing 4583: NoHttpRazorContext should handle more use cases

--- Composite/AspNet/Razor/NoHttpRazorRequest.cs
+++ Composite/AspNet/Razor/NoHttpRazorRequest.cs
@@ -1,12 +1,87 @@
-﻿using System.Web;
+﻿using System;
+using System.Collections.Specialized;
+using System.Web;
 
 namespace Composite.AspNet.Razor
 {
     internal class NoHttpRazorRequest : HttpRequestBase
-	{
-		public override bool IsLocal
+    {
+        private NameValueCollection _form;
+        private NameValueCollection _queryString;
+        private NameValueCollection _headers;
+        private NameValueCollection _params;
+        private NameValueCollection _serverVariables;
+        private HttpCookieCollection _cookies;
+
+        public override HttpCookieCollection Cookies
+        {
+            get { return _cookies ?? (_cookies = new HttpCookieCollection()); }
+        }
+
+        public override bool IsLocal
 		{
 			get { return false; }
 		}
-	}
+
+        public override NameValueCollection Form
+        {
+            get { return _form ?? (_form = new NameValueCollection()); }
+        }
+
+        public override NameValueCollection Headers
+        {
+            get { return _headers ?? (_headers = new NameValueCollection());}
+        }
+
+        public override string HttpMethod
+        {
+            get { return "GET"; }
+        }
+
+        public override bool IsAuthenticated
+        {
+            get { return false; }
+        }
+
+        public override bool IsSecureConnection
+        {
+            get { return false; }
+        }
+
+        public override string this[string key]
+        {
+            get { return null; }
+        }
+
+        public override NameValueCollection Params
+        {
+            get { return _params ?? (_params = new NameValueCollection()); }
+        }
+
+        public override string PathInfo
+        {
+            get { return null; }
+        }
+
+        public override NameValueCollection QueryString
+        {
+            get { return _queryString ?? (_queryString = new NameValueCollection()); }
+        }
+
+        public override string RequestType 
+        {
+            get { return HttpMethod; }
+            set { throw new NotSupportedException();} 
+        }
+
+        public override NameValueCollection ServerVariables
+        {
+            get { return _serverVariables ?? (_serverVariables = new NameValueCollection()); }
+        }
+
+        public override string UserAgent
+        {
+            get { return ""; }
+        }
+    }
 }
