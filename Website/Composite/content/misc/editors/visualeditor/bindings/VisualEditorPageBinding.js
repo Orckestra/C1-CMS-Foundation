73ab26134c0b494505a6425c32ff83295b2ad6bf
Taras Nakonechnyi <neexite@gmail.com>
2014-06-20 13:58:15 +0200

    Bugfix 4718 CRITICAL: Pages get unsaved occasionally with being edited

--- Website/Composite/content/misc/editors/visualeditor/bindings/VisualEditorPageBinding.js
+++ Website/Composite/content/misc/editors/visualeditor/bindings/VisualEditorPageBinding.js
@@ -510,7 +510,12 @@ VisualEditorPageBinding.prototype.getCheckSum = function (checksum) {
 			return checksum;
 		}
 	} else {
-		checksum = this._tinyInstance.getDoc().body.innerHTML;
+		// IE innerHTML - returns wrong quotes (' instead ") in attribute data-markup
+		if (Client.isExplorer || Client.isExplorer11) {
+			checksum = this._tinyInstance.getContent();
+		} else {
+			checksum = this._tinyInstance.getDoc().body.innerHTML;
+		}
 
 		//delete mceC1Focused from checksum to prevent unexpected dirty
 		checksum = checksum.replace(/\s*mceC1Focused\s*/g, "");
