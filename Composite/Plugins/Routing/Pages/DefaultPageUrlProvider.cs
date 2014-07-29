133032302751733c8c0a8ba1e1a76ef19942c373
Marcus Wendt <marcus@wendt.dk>
2014-07-29 14:34:53 +0200

    Fixing SQL & Friendly URL issue - see https://composite.zendesk.com/agent/#/tickets/7380 and http://stackoverflow.com/questions/1368946/linq-syntax-where-string-value-is-not-null-or-empty
    Fixing exception "Method 'Boolean IsNullOrEmpty(System.String)' has no supported translation to SQL"

--- Composite/Plugins/Routing/Pages/DefaultPageUrlProvider.cs
+++ Composite/Plugins/Routing/Pages/DefaultPageUrlProvider.cs
@@ -388,7 +388,7 @@ namespace Composite.Plugins.Routing.Pages
             return _friendlyUrls.GetOrAddSync(scopeKey, a =>
             {
                 var result = new Hashtable<string, Guid>();
-                foreach (var pair in DataFacade.GetData<IPage>().Where(p => !string.IsNullOrEmpty(p.FriendlyUrl))
+                foreach (var pair in DataFacade.GetData<IPage>().Where(p => !(p.FriendlyUrl == null && p.FriendlyUrl.Equals(string.Empty)))
                     .Select(p => new {p.Id, p.FriendlyUrl}))
                 {
                     result[pair.FriendlyUrl.ToLowerInvariant()] = pair.Id;
