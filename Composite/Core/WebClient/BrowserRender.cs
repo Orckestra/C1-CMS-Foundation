1e0e7c061966944f272eb9ba8ebd6346bc627d2b
Marcus Wendt <marcus@wendt.dk>
2015-02-12 12:59:42 +0100

    Fiddling with the PhantomJs pre init delay - trying to hit a sweet spot which goes down well with SaaS first time experience

--- Composite/Core/WebClient/BrowserRender.cs
+++ Composite/Core/WebClient/BrowserRender.cs
@@ -40,7 +40,7 @@ namespace Composite.Core.WebClient
         private static DateTime _lastUsageDate = DateTime.MinValue;
         private static readonly TimeSpan RecycleOnIdleInterval = TimeSpan.FromSeconds(30); 
         private const int RecycleTimerInterval_ms = 10000;
-        private const int EnsureReadinessDelay_ms = 500;
+        private const int EnsureReadinessDelay_ms = 3000;
 
         private static volatile bool Enabled = true;
         private static volatile bool ServerAvailabilityChecked;
