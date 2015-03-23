09ef3ddd4130d42a4fdd0403c180ddaa1d56ea28
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2015-03-23 13:49:36 +0100

    Adding [DecimalRangeValidator] and [NullDecimalRangeValidator] attributes

---
+++ Composite/Data/Validation/Validators/DecimalRangeValidatorAttribute.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace Composite.Data.Validation.Validators
+{
+    /// <summary>
+    /// Validator rule for data type properties.
+    /// Validate that a decimal field has a value than falls within a minimum and maximum value. 
+    /// </summary>    
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Method | AttributeTargets.Parameter, AllowMultiple = true, Inherited = false)]
+    public sealed class DecimalRangeValidatorAttribute : Microsoft.Practices.EnterpriseLibrary.Validation.Validators.ValueValidatorAttribute
+    {
+        /// <summary>
+        /// Validator rule for data type properties.
+        /// Validate that a decimal field has a value than falls within a minimum and maximum value. 
+        /// </summary>    
+        public DecimalRangeValidatorAttribute(decimal lowerBound, decimal upperBound)
+        {
+            this.LowerBound = lowerBound;
+            this.UpperBound = upperBound;
+        }
+
+
+        /// <exclude />
+        public decimal LowerBound
+        {
+            get;
+            private set;
+        }
+
+
+        /// <exclude />
+        public decimal UpperBound
+        {
+            get;
+            private set;
+        }
+
+
+        /// <exclude />
+        protected override Microsoft.Practices.EnterpriseLibrary.Validation.Validator DoCreateValidator(Type targetType)
+        {
+            return new Microsoft.Practices.EnterpriseLibrary.Validation.Validators.RangeValidator<decimal>(
+                this.LowerBound,
+                Microsoft.Practices.EnterpriseLibrary.Validation.Validators.RangeBoundaryType.Inclusive,
+                this.UpperBound,
+                Microsoft.Practices.EnterpriseLibrary.Validation.Validators.RangeBoundaryType.Inclusive);
+        }
+    }
+}
---
+++ Composite/Data/Validation/Validators/NullDecimalRangeValidatorAttribute.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.Practices.EnterpriseLibrary.Validation;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+
+
+namespace Composite.Data.Validation.Validators
+{
+    /// <summary>
+    /// Validator rule for data type properties.
+    /// Validate that an nullable decimal - when not null - has a value than falls within a minimum and maximum value. 
+    /// </summary>    
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Method | AttributeTargets.Parameter, AllowMultiple = true, Inherited = false)]
+    public sealed class NullDecimalRangeValidatorAttribute : ValueValidatorAttribute
+    {
+        /// <summary>
+        /// Validator rule for data type properties.
+        /// Validate that an nullable decimal - when not null - has a value than falls within a minimum and maximum value. 
+        /// </summary>    
+        public NullDecimalRangeValidatorAttribute(decimal lowerBound, decimal upperBound)
+        {
+            this.LowerBound = lowerBound;
+            this.UpperBound = upperBound;
+        }
+
+
+        /// <exclude />
+        public decimal LowerBound
+        {
+            get;
+            private set;
+        }
+
+
+        /// <exclude />
+        public decimal UpperBound
+        {
+            get;
+            private set;
+        }
+
+
+        /// <exclude />
+        protected override Validator DoCreateValidator(Type targetType)
+        {
+            return new NullableDecimalRangeValidator(new RangeValidator<decimal>(
+                this.LowerBound, RangeBoundaryType.Inclusive,
+                this.UpperBound, RangeBoundaryType.Inclusive));
+        }
+
+        private class NullableDecimalRangeValidator : Validator<decimal?>
+        {
+            private readonly RangeValidator<decimal> _innerValidator;
+
+            public NullableDecimalRangeValidator(RangeValidator<decimal> innerValidator)
+                : base(null, null)
+            {
+                _innerValidator = innerValidator;
+            }
+
+            protected override string DefaultMessageTemplate
+            {
+                get { return _innerValidator.MessageTemplate; }
+            }
+
+            protected override void DoValidate(decimal? objectToValidate, object currentTarget, string key, ValidationResults validationResults)
+            {
+                if (objectToValidate == null)
+                {
+                    return;
+                }
+
+                validationResults.AddAllResults(_innerValidator.Validate(objectToValidate.Value));
+            }
+        }
+    }
+}
