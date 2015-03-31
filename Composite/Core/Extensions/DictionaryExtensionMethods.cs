bd99d9ce6fc6e99744cbe790540fe337ad8c4264
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2015-03-31 12:39:47 +0200

    Dictionary extension - refactoring

--- Composite/Core/Extensions/DictionaryExtensionMethods.cs
+++ Composite/Core/Extensions/DictionaryExtensionMethods.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,22 +8,29 @@ namespace Composite.Core.Extensions
 {
     internal static class DictionaryExtensionMethods
     {
-        private static readonly object _addLock = new object();
-
         public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> createValue)
         {
-            TValue value;
-            if (!dictionary.TryGetValue(key, out value))
+            if (dictionary is ConcurrentDictionary<TKey, TValue>)
             {
-                lock (_addLock)
-                {
-                    if (!dictionary.TryGetValue(key, out value))
-                    {
-                        value = createValue();
-                        dictionary.Add(key, value);
-                    }
+                var concurrentDictionary = dictionary as ConcurrentDictionary<TKey, TValue>;
+                return concurrentDictionary.GetOrAdd(key, k => createValue());
+            }
 
+            TValue value;
+            if (dictionary.TryGetValue(key, out value))
+            {
+                return value;
+            }
+
+            lock (dictionary)
+            {
+                if (dictionary.TryGetValue(key, out value))
+                {
+                    return value;
                 }
+
+                value = createValue();
+                dictionary.Add(key, value);
             }
 
             return value;
