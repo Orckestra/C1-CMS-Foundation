283c0c1641f68182046b0a3e890132e0af8e4375
Inna Boitsun <inna.boytsun.85@gmail.com>
2015-06-25 14:32:36 +0200

    added GenerateIconSprite.aspx to the project

---
+++ Website/Composite/GenerateIconSprite.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System.Xml;
+using Composite.Core.IO;
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+public partial class GenerateIconSprite : System.Web.UI.Page
+{
+
+    private string svgBlank = @"<?xml version=""1.0"" encoding=""UTF-8"" standalone=""no""?><svg version=""1.1"" xmlns=""http://www.w3.org/2000/svg"" xmlns:xlink=""http://www.w3.org/1999/xlink"" width=""24"" height=""24""  viewBox=""0 0 128 128"">
+    <defs></defs></svg>";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var pathSVGIcons = Server.MapPath("~/Composite/images/icons/svg/");
+
+        XNamespace svgNamespace = @"http://www.w3.org/2000/svg";
+
+        var svgSprite = XElement.Parse(svgBlank);
+
+        var defs = svgSprite.Descendants(svgNamespace + "defs").First();
+
+        foreach (var file in Directory.GetFiles(pathSVGIcons))
+        {
+            var id = Path.GetFileNameWithoutExtension(file);
+            var svgElement = XElement.Load(file);
+
+         
+
+            var taggetGroup = svgElement.Descendants(svgNamespace + "g").FirstOrDefault(el => el.Attribute("id") != null && el.Attribute("id").Value == id);
+
+            if (taggetGroup != null)
+            {
+                defs.Add(taggetGroup);
+            }
+        }
+
+
+       XmlWriterSettings settings = new XmlWriterSettings();
+        settings.OmitXmlDeclaration = true;
+        StringWriter sw = new StringWriter();
+        using (XmlWriter xw = XmlWriter.Create(PathUtil.Resolve("~/Composite/images/sprite.svg"), settings))
+        {
+            svgSprite.Save(xw);
+        }
+   
+    }
+}
