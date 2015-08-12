471bd06571a37824546d4054d75cc5c15dc176f3
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2015-08-12 15:51:09 +0200

    Fixing the build

--- Website/Composite/top.aspx.designer.cs
+++ Website/Composite/top.aspx.designer.cs
@@ -37,4 +37,13 @@ public partial class Composite_Management_Top {
     /// To modify move field declaration from designer file to code-behind file.
     /// </remarks>
     protected global::System.Web.UI.WebControls.PlaceHolder splashholder;
+
+    /// <summary>
+    /// splashholder control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.PlaceHolder gruntholder;
 }
