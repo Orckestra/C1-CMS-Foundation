f30cbc74df3c5978a90798352fde16af48f57241
Marcus Wendt <marcus@wendt.dk>
2015-08-12 10:33:56 +0200

    Setting version moniker to "Composite C1 5.0 (alpha)"

--- Composite/Properties/AssemblyInfo.cs
+++ Composite/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 #if !InternalBuild
-[assembly: AssemblyTitle("Composite C1 4.4")] 
+[assembly: AssemblyTitle("Composite C1 5.0 (alpha)")] 
 #else
-[assembly: AssemblyTitle("Composite C1 4.4 (Internal Build)")]
+[assembly: AssemblyTitle("Composite C1 5.0 (Internal Build)")]
 #endif
 
 [assembly: AssemblyDescription("Composite C1 Core classes")]
@@ -39,7 +39,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("4.4.*")]
+[assembly: AssemblyVersion("5.0.*")]
 
 [assembly: InternalsVisibleTo("UpgradePackage")]
 [assembly: InternalsVisibleTo("Composite.Workflows")]
