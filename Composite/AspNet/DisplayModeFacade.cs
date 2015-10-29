35cc2602501886353b4c4f2e3161dc8da4876c07
Pauli Østerø <poe@cphcloud.com>
2015-10-29 14:48:07 +0100

    Removed PrintMode on DisplayModeProvider
    Since DisplayModeProvider is a public class included in the WebPages library, there is no need for C1 to provide a PrintMode out-of-the-box, since sites that need this, or other special modes, are able to add them manually.

--- Composite/AspNet/DisplayModeFacade.cs
+++ Composite/AspNet/DisplayModeFacade.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.WebPages;
+﻿using System.Web.WebPages;
 
 namespace Composite.AspNet
 {
@@ -11,23 +10,6 @@ namespace Composite.AspNet
             {
                 ContextCondition = ctx => ctx.GetOverriddenBrowser().IsMobileDevice
             });
-
-            DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode("print")
-            {
-                ContextCondition = PrintCondition
-            });
-
-            DisplayModeProvider.Instance.Modes.Insert(0, new LegacyC1DisplayMode("print")
-            {
-                ContextCondition = PrintCondition
-            });
-        }
-
-        private static bool PrintCondition(HttpContextBase ctx)
-        {
-            var qs = ctx.Request.QueryString;
-
-            return qs.AllKeys.Length > 0 && qs.Keys[0] == null && qs[0] == "print";
         }
     }
 }
