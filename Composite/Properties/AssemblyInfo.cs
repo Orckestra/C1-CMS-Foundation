a119725b4641b14482e1f05690d2b865b9ba325f
CompositeC1 <maw@composite.net>
2015-11-30 14:02:20 +0100

    Updating version moniker to "Composite C1 5.0"

--- Composite/Properties/AssemblyInfo.cs
+++ Composite/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 #if !InternalBuild
-[assembly: AssemblyTitle("Composite C1 5.0 (Beta 1)")] 
+[assembly: AssemblyTitle("Composite C1 5.0")] 
 #else
-[assembly: AssemblyTitle("Composite C1 5.0 (Beta 1, Internal Build)")]
+[assembly: AssemblyTitle("Composite C1 5.0 (Internal Build)")]
 #endif
 
 [assembly: AssemblyDescription("Composite C1 Core classes")]
