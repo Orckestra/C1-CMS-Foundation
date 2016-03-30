628830406037364cc901b24fab3c6344ee7604db
neexite <neexite@gmail.com>
2016-03-30 13:33:20 +0300

    Fix #122 Validation messages "stick" when you switch between the tabs #122 +fix dublicate ballons

--- Website/Composite/scripts/source/top/ui/bindings/balloons/BalloonBinding.js
+++ Website/Composite/scripts/source/top/ui/bindings/balloons/BalloonBinding.js
@@ -3,20 +3,20 @@ BalloonBinding.prototype.constructor = BalloonBinding;
 BalloonBinding.superclass = Binding.prototype;
 
 /**
- * Time before each position update in milliseconds. 
+ * Time before each position update in milliseconds.
  * Also the timeout before balloon is shown, for some reason.
  */
 BalloonBinding.TIMEOUT = parseInt ( 200 );
 
 /*
- * Position offsets. Attempting to place 
+ * Position offsets. Attempting to place
  * the balloon elegantly relative to binding.
  */
 BalloonBinding.OFFSET_X =  parseInt ( 14 );
 BalloonBinding.OFFSET_Y =  parseInt ( 6 );
 
 /*
- * 
+ *
  */
 BalloonBinding.ACTION_SNAP = "balloon snap";
 
@@ -26,8 +26,8 @@ BalloonBinding.ACTION_SNAP = "balloon snap";
 BalloonBinding.CLASSNAME_LEFT = "left";
 
 /**
- * Used to locate an appropriate balloon environment. 
- * TODO: Design para-frame binding type locator? For now, 
+ * Used to locate an appropriate balloon environment.
+ * TODO: Design para-frame binding type locator? For now,
  * environments need to be made aware of the BallonBinding.
  */
 BalloonBinding.ACTION_INITIALIZE = "ballon initialize";
@@ -41,15 +41,15 @@ function BalloonBinding () {
 	 * @type {SystemLogger}
 	 */
 	this.logger = SystemLogger.getLogger ( "BalloonBinding" );
-	
+
 	/**
 	 * We snap to this binding.
 	 * @type {IData}
 	 */
 	this._snapTargetBinding = null;
-	
+
 	/**
-	 * We nondisplay the balloon when outside the boundaries of this binding. 
+	 * We nondisplay the balloon when outside the boundaries of this binding.
 	 * Typically a DockBinding, a DialogBinding or a ScrollBoxBinding.
 	 * @type {Binding}
 	 */
@@ -65,7 +65,7 @@ function BalloonBinding () {
  * Identifies binding.
  */
 BalloonBinding.prototype.toString = function () {
-	
+
 	return "[BalloonBinding]";
 }
 
@@ -75,29 +75,29 @@ BalloonBinding.prototype.toString = function () {
  * @overloads {Binding#onBindintAttach}
  */
 BalloonBinding.prototype.onBindingAttach = function () {
-	
+
 	BalloonBinding.superclass.onBindingAttach.call ( this );
-	
+
 	this.addActionListener ( Binding.ACTION_ACTIVATED );
 	this.addActionListener ( ControlBinding.ACTION_COMMAND );
-	
+
 	/*
 	 * Build close button.
 	 */
 	this._controlGroupBinding = this.add (
-		ControlGroupBinding.newInstance ( this.bindingDocument ) 
+		ControlGroupBinding.newInstance ( this.bindingDocument )
 	);
 	var controlBinding = DialogControlBinding.newInstance ( this.bindingDocument );
 	controlBinding.setControlType ( ControlBinding.TYPE_CLOSE );
 	this._controlGroupBinding.add ( controlBinding );
 	this._controlGroupBinding.attachRecursive ();
-	
+
 	/*
 	 * Build speak elements.
 	 */
 	var element = DOMUtil.createElementNS ( Constants.NS_UI, "ui:balloonspeak", this.bindingDocument );
 	this.bindingElement.appendChild ( element );
-	
+
 	var label = this.getLabel ();
 	if ( label != null ) {
 		this.setLabel ( label );
@@ -108,14 +108,14 @@ BalloonBinding.prototype.onBindingAttach = function () {
  * @overloads {Binding#onBindintAttach}
  */
 BalloonBinding.prototype.onBindingDispose = function () {
-	
+
 	BalloonBinding.superclass.onBindingDispose.call ( this );
-	
+
 	if ( this._updateInterval ) {
 		window.clearInterval ( this._updateInterval );
 		this._updateInterval = null;
 	}
-	
+
 	var binding = this._snapTargetBinding;
 	if ( Binding.exists ( binding ) == true ) {
 		binding.removeActionListener ( Binding.ACTION_BLURRED, this );
@@ -128,21 +128,21 @@ BalloonBinding.prototype.onBindingDispose = function () {
  * @param {IData} binding
  */
 BalloonBinding.prototype.snapTo = function ( binding ) {
-	
+
 	if ( Interfaces.isImplemented ( IData, binding )) {
-		
+
 		this._snapTargetBinding = binding;
-		
+
 		var action = binding.dispatchAction ( BalloonBinding.ACTION_INITIALIZE );
 		if ( action && action.isConsumed ) {
 			this._environmentBinding = action.listener;
 		}
 		if ( this._environmentBinding ) {
-			
+
 			binding.addActionListener ( Binding.ACTION_BLURRED, this );
 			binding.addActionListener ( Binding.ACTION_VALID, this );
 			this.subscribe ( BroadcastMessages.VIEW_CLOSED );
-		
+
 			/*
 			 * Position and update on timed interval.
 			 * TODO: why is the position wrong if we invoke _updatePosition now?
@@ -155,50 +155,55 @@ BalloonBinding.prototype.snapTo = function ( binding ) {
 					self.dispose ();
 				}
 			}, BalloonBinding.TIMEOUT );
-			
+
 			/*
-			 * Note that the target, not the balloon, 
+			 * Note that the target, not the balloon,
 			 * is dispatching this action...
 			 */
 			binding.dispatchAction ( BalloonBinding.ACTION_SNAP );
-			
+
 		} else {
 			throw "No environment fit for balloons!";
 		}
 	}
 }
 
+BalloonBinding.prototype.isSnapTo = function (binding) {
+
+	return binding === this._snapTargetBinding;
+}
+
 /**
  * @param {Point} point
  */
 BalloonBinding.prototype._updatePosition = function () {
-	
+
 	var target = this._snapTargetBinding;
 	var environment = this._environmentBinding;
 	var root = UserInterface.getBinding ( target.bindingDocument.body );
-	
+
 	if ( Binding.exists ( target ) && Binding.exists ( environment )) {
-		
+
 		if ( !root.isActivated ) {
 			if ( this.isVisible == true ) {
 				this.hide ();
 			}
 		} else if ( target.isAttached && environment.isAttached ) {
-		
+
 			var tPoint = target.boxObject.getUniversalPosition ();
 			var ePoint = environment.boxObject.getUniversalPosition ();
-			
+
 			ePoint.y += environment.bindingElement.scrollTop;
 			ePoint.x += environment.bindingElement.scrollLeft;
-			
+
 			var tDim = target.boxObject.getDimension ();
 			var eDim = environment.boxObject.getDimension ();
-			
+
 			/*
-			 * Calculations to undispay balloon if target is not 
+			 * Calculations to undispay balloon if target is not
 			 * visible within the boudaries of the environment.
 			 */
-			var isAbort = false;	
+			var isAbort = false;
 			if ( tPoint.y + tDim.h < ePoint.y ) {
 				isAbort = true;
 			} else if ( tPoint.x + tDim.w < ePoint.x ) {
@@ -208,7 +213,7 @@ BalloonBinding.prototype._updatePosition = function () {
 			} else if ( tPoint.x > ePoint.x + eDim.w ) {
 				isAbort = true;
 			}
-			
+
 			if ( !isAbort ) {
 				this._setComputedPosition ( tPoint, ePoint, tDim, eDim );
 				if ( !this.isVisible ) {
@@ -231,12 +236,12 @@ BalloonBinding.prototype._updatePosition = function () {
  * @param {Dimension} eDim
  */
 BalloonBinding.prototype._setComputedPosition = function ( tPoint, ePoint, tDim, eDim ) {
-	
+
 	var wDim = WindowManager.getWindowDimensions ();
 	var bDim = this._getDimension ();
 	var point = tPoint;
 	var isLeft = false;
-	
+
 	/*
 	 * Display balloon on the left side?
 	 */
@@ -245,7 +250,7 @@ BalloonBinding.prototype._setComputedPosition = function ( tPoint, ePoint, tDim,
 	} else if ( tPoint.x + tDim.w >= ePoint.x + eDim.w ) { // target cut by environment box (on the right side)?
 		isLeft = true;
 	}
-	
+
 	if ( isLeft ) {
 		point.x -= ( bDim.w + BalloonBinding.OFFSET_X );
 		this.attachClassName ( BalloonBinding.CLASSNAME_LEFT );
@@ -253,30 +258,31 @@ BalloonBinding.prototype._setComputedPosition = function ( tPoint, ePoint, tDim,
 		point.x += tDim.w + BalloonBinding.OFFSET_X;
 	 	this.detachClassName ( BalloonBinding.CLASSNAME_LEFT );
 	}
-	
+
 	/*
-	 * Make the balloon appear above the target. 
+	 * Make the balloon appear above the target.
 	 */
 	point.y -= ( bDim.h );
 	point.y += BalloonBinding.OFFSET_Y;
-	
+	point.y = Math.round(point.y);
+
 	this._setPosition ( point );
 }
 
 /**
- * Dispose the balloon when associated view closes and 
- * whenever a new view is opened (otherwise the balloon 
- * may block the opened view). 
+ * Dispose the balloon when associated view closes and
+ * whenever a new view is opened (otherwise the balloon
+ * may block the opened view).
  * @implements {IBroadcastListener}
  * @param {string} broadcast
  * @param {object} arg
  */
 BalloonBinding.prototype.handleBroadcast = function ( broadcast, arg ) {
-	
+
 	BalloonBinding.superclass.handleBroadcast.call ( this, broadcast, arg );
-	
+
 	switch ( broadcast ) {
-	
+
 		/*
 		 * If our view closed, close the balloon now.
 		 * Don't wait for interval to find out.
@@ -295,7 +301,7 @@ BalloonBinding.prototype.handleBroadcast = function ( broadcast, arg ) {
  * @return {boolean}
  */
 BalloonBinding.prototype._isAssociatedView = function ( handle ) {
-	
+
 	var result = false;
 	if ( this._snapTargetBinding ) {
 		var view = this._snapTargetBinding.getAncestorBindingByType ( ViewBinding, true );
@@ -307,20 +313,21 @@ BalloonBinding.prototype._isAssociatedView = function ( handle ) {
 }
 
 /**
- * Set position. This has been fitted with a mechanism that 
+ * Set position. This has been fitted with a mechanism that
  * doesn't update the position while CSS transforms are running.
  * @param {Point} point
  */
 BalloonBinding.prototype._setPosition = function ( point ) {
-	
+
 	var isAbort = false;
 	var pos = this.boxObject.getLocalPosition ();
 	if ( this._point != null ) {
 		if ( pos.x != this._point.x || pos.y != this._point.y ) {
 			isAbort = true;
-		}   
+		}
 	}
-	if ( !isAbort ) {
+
+	if (!isAbort) {
 		this.bindingElement.style.left = point.x + "px";
 		this.bindingElement.style.top = point.y + "px";
 		this._point = point;
@@ -331,30 +338,30 @@ BalloonBinding.prototype._setPosition = function ( point ) {
  * @return {Point}
  */
 BalloonBinding.prototype._getPosition = function () {
-	
-	return new Point ( 
+
+	return new Point (
 		this.bindingElement.offsetLeft,
 		this.bindingElement.offsetTop
 	);
 }
- 
+
  /**
  * @return {Dimension}
  */
 BalloonBinding.prototype._getDimension = function () {
-	
-	return new Dimension ( 
+
+	return new Dimension (
 		this.bindingElement.offsetWidth,
 		this.bindingElement.offsetHeight
 	);
 }
 
-/** 
+/**
  * Cannot use "display" property because we need to compute on width and height.
  * @overwrites {Binding#hide}
  */
 BalloonBinding.prototype.hide = function () {
-	
+
 	if ( this.isVisible ) {
 		this.bindingElement.style.visibility = "hidden";
 		this.isVisible = false;
@@ -365,7 +372,7 @@ BalloonBinding.prototype.hide = function () {
  * @overwrites {Binding#show}
  */
 BalloonBinding.prototype.show = function () {
-	
+
 	if ( !this.isVisible ) {
 		this.bindingElement.style.visibility = "visible";
 		this.isVisible = true;
@@ -378,30 +385,30 @@ BalloonBinding.prototype.show = function () {
  * @param {Action} action
  */
 BalloonBinding.prototype.handleAction = function ( action ) {
-	
+
 	BalloonBinding.superclass.handleAction.call ( this, action );
-	
+
 	var binding = action.target;
-	
+
 	switch ( action.type ) {
-	
+
 		case Binding.ACTION_ACTIVATED :
 			if ( this._snapTargetBinding ) {
-				this._snapTargetBinding.dispatchAction ( 
-					Binding.ACTION_ACTIVATED 
+				this._snapTargetBinding.dispatchAction (
+					Binding.ACTION_ACTIVATED
 				);
 				action.consume ();
 			}
-	
+
 		/**
-		 * Note that the binding may be blurred when the server (not the  
-		 * client) deems it invalid. In that case, a dialog may be opened,  
-		 * blurring the binding, but the call to method validate will 
-		 * still return true (because only the server knows for sure). 
+		 * Note that the binding may be blurred when the server (not the
+		 * client) deems it invalid. In that case, a dialog may be opened,
+		 * blurring the binding, but the call to method validate will
+		 * still return true (because only the server knows for sure).
 		 * In that case, we have to make sure not to dispose the balloon.
 		 */
 		case Binding.ACTION_BLURRED :
-		case Binding.ACTION_VALID :	
+		case Binding.ACTION_VALID :
 			if ( binding == this._snapTargetBinding ) {
 				var self = this;
 				setTimeout ( function () {
@@ -423,34 +430,39 @@ BalloonBinding.prototype.handleAction = function ( action ) {
 				}, 0 );
 			}
 			break;
-			
+
 		case ControlBinding.ACTION_COMMAND :
 			this.dispose ();
 			break;
 	}
 }
 
-/** 
+/**
  * Set label
  * @param {string} label
  */
 BalloonBinding.prototype.setLabel = function ( label ) {
-	
-	if ( this.isAttached == true ) {
-		var element = DOMUtil.createElementNS ( Constants.NS_UI, "ui:balloontext", this.bindingDocument );
+
+	if (this.isAttached) {
+		if (!this.shadowTree.balloontext) {
+			this.shadowTree.balloontext = DOMUtil.createElementNS(Constants.NS_UI, "ui:balloontext", this.bindingDocument);
+			this.bindingElement.appendChild(this.shadowTree.balloontext);
+		}
+		while (this.shadowTree.balloontext.firstChild) {
+			this.shadowTree.balloontext.removeChild(this.shadowTree.balloontext.firstChild);
+		}
 		var text = this.bindingDocument.createTextNode ( label );
-		element.appendChild ( text );
-		this.bindingElement.appendChild(element);
+		this.shadowTree.balloontext.appendChild(text);
 	}
 	this.setProperty ( "label", label );
 }
 
-/** 
+/**
  * Get label.
  * @return {string}
  */
 BalloonBinding.prototype.getLabel = function () {
-	
+
 	return this.getProperty ( "label" );
 }
 
