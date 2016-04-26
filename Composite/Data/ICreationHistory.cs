460611dc59aaab5bea34dd2a442a74a17eca2036
Marcus Wendt <marcus@wendt.dk>
2016-04-26 13:22:16 +0200

    Making PackageFragmentInstaller for data take default value attribute into account when validating data to install. Adding default value attribute to to ICreationHistory.

--- Composite/Data/ICreationHistory.cs
+++ Composite/Data/ICreationHistory.cs
@@ -12,6 +12,7 @@ namespace Composite.Data
         [StoreFieldType(PhysicalStoreFieldType.DateTime)]
         [ImmutableFieldId("{59E10FE8-EC55-4b10-B17A-FDE3EB4690F1}")]
         [DefaultFieldNowDateTimeValue()]
+        [FunctionBasedNewInstanceDefaultFieldValue("<f:function xmlns:f=\"http://www.composite.net/ns/function/1.0\" name=\"Composite.Utils.Date.Now\" />")]
         [FieldPosition(502)]
         DateTime CreationDate { get; set; }
 
