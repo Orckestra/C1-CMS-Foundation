cd327b68dac477136f40ccad27c089f47cfbf540
Morteza Kasravi <kasravi@gmail.com>
2016-05-11 10:52:40 +0200

    to attached desired versioning setting from url path this http module is defined. the folowing line should be added to website's web.config <add name="VersionHttpModule" type="Composite.VersionPublishing.VersionHttpModule, Composite.VersionPublishing" />

---
+++ Composite.VersionPublishing/VersionHttpModule.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Web;
+using Composite.Core.Extensions;
+using Composite.Core.Routing.Pages;
+using Composite.Data;
+
+namespace Composite.VersionPublishing
+{
+    class VersionHttpModule : IHttpModule
+    {
+        private DataConnection _dataConnection;
+
+        private void SetVersioningServicefromUrlPath(string pathInfo)
+        {
+            if (VersionNameUrlHelper.CheckIfAdminUrl(pathInfo))
+            {
+                _dataConnection.AddService(VersioningServiceSettings.NoFiltering());
+                return;
+            }
+            var versionName = VersionNameUrlHelper.ResolveVersionName(pathInfo);
+            if (!versionName.IsNullOrEmpty())
+            {
+                C1PageRoute.RegisterPathInfoUsage();
+                _dataConnection.AddService(VersioningServiceSettings.ByName(versionName));
+            }
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += context_BeginRequest;
+            context.EndRequest += context_EndRequest;
+        }
+
+        private void context_EndRequest(object sender, EventArgs e)
+        {
+            _dataConnection.Dispose();
+        }
+
+        private void context_BeginRequest(object sender, EventArgs e)
+        {
+            var httpApplication = sender as HttpApplication;
+            if (httpApplication == null) return;
+
+            _dataConnection = new DataConnection();
+            var httpContext = httpApplication.Context;
+            SetVersioningServicefromUrlPath(httpContext.Request.Path);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
