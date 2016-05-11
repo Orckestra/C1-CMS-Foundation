cdf1fa382e325bb0f234d4ca9492b3f7b3ba2188
Morteza Kasravi <kasravi@gmail.com>
2016-05-11 11:00:17 +0200

    registering service mapper and setting default service

--- Composite.VersionPublishing/StartupHandler.cs
+++ Composite.VersionPublishing/StartupHandler.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Composite.C1Console.Elements;
+using Composite.Core.Application;
 
 namespace Composite.VersionPublishing
 {
+    [ApplicationStartup]
     public class StartupHandler
     {
         public static void OnBeforeInitialize()
         {
+            VersionPublishingFacade.RegisterDefaultVersioningService(VersioningServiceSettings.MostRelevant());
+            UrlToEntityTokenFacade.Register(new VersionedDataUrlToEntityTokenMapper());
         }
 
         public static void OnInitialized()
