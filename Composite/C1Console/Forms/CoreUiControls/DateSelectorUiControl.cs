50984d5084f719713404cd8e493517b58b0e5159
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2016-05-30 10:14:03 +0200

    Adding "Required" attribute to <DateTimeSelector /> form control

--- Composite/C1Console/Forms/CoreUiControls/DateSelectorUiControl.cs
+++ Composite/C1Console/Forms/CoreUiControls/DateSelectorUiControl.cs
@@ -8,11 +8,14 @@ namespace Composite.C1Console.Forms.CoreUiControls
     [ControlValueProperty("Date")]
     internal abstract class DateTimeSelectorUiControl : UiControl
     {
-        [BindableProperty()]
-        [FormsProperty()]
+        [BindableProperty]
+        [FormsProperty]
         public DateTime? Date { get; set; }
 
-        [FormsProperty()]
+        [FormsProperty]
         public bool ReadOnly { get; set; }
+
+        [FormsProperty]
+        public bool Required { get; set; }
     }
 }
