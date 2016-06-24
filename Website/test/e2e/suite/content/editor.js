5960dfa61ff537213909642aecb8623bc2e0ae52
Gert Sønderby <gert.sonderby@gmail.com>
2016-06-24 17:00:21 +0200

    Put some comments in editor test to make it make more sense.

--- Website/test/e2e/suite/content/editor.js
+++ Website/test/e2e/suite/content/editor.js
@@ -4,18 +4,30 @@ module.exports = {
 		var content = browser.page.content();
 		content
 			.prepare()
-			.section.docktabs.click('docktab:nth-of-type(1)'); // Select first tab
+			// Select first tab
+			.section.docktabs.click('docktab:nth-of-type(1)');
 		content
 			.enterFrame('@browserFrame')
-			.click('toolbarbutton[image="page-edit-page"]') // Click edit button
+			// Click edit button (identified by icon name)
+			.click('toolbarbutton[image="page-edit-page"]')
 			.leaveFrame()
-			.waitForElementPresent('view:nth-of-type(2) window', 1000) // Locate and check editor screen - relies on it being the second ui:window tag in existence in this frame.
+			// Locate and check editor screen
+			// relies on it being the second ui:window tag in existence in this frame.
+			.waitForElementPresent('view:nth-of-type(2) window', 1000)
 			.enterFrame('view:nth-of-type(2) window iframe')
 			.waitForElementVisible('iframe[src="/Composite/content/misc/editors/visualeditor/visualeditor.aspx?config=common"]', 1000);
 		browser.pause(500);
 		content
-			.selectFrame('#tinymce > img:nth-child(1)') // Find frame with editor content
-			.verify.visible('#tinymce > img:nth-child(2)') // Check that it has more than just one entry
+			// Find frame with editor content
+			.selectFrame('#tinymce > img:nth-child(1)')
+			// Check that it has more than just one entry
+			.verify.visible('#tinymce > img:nth-child(2)')
+			// Enter the element
+			.click('#tinymce > img:nth-child(2)')
+		browser
+			.pause(3000)
+		content
+			// Close editor after you
 			.enter()
 			.section.docktabs.closeTab(2);
 		browser.end();
