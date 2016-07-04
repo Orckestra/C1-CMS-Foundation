cf07297941a798870a33da40be6896a014aca21e
Gert Sønderby <gert.sonderby@gmail.com>
2016-07-04 10:50:20 +0200

    Added sinon, testing button clicks.

--- Website/test/unit/helpers/expect.js
+++ Website/test/unit/helpers/expect.js
@@ -1,15 +1,12 @@
-require( './emulateDom');
+import './emulateDom';
 
-var unexpected = require('unexpected');
-
-// then require unexpected-react
-var unexpectedReact = require('unexpected-react');
-
-// then react
-var React = require('react');
+import unexpected from 'unexpected';
+import unexpectedReact from 'unexpected-react';
+import unexpectedSinon from 'unexpected-sinon';
 
 // define our instance of the `expect` function to use
 const expect = unexpected.clone()
-    .use(unexpectedReact);
+	.use(unexpectedReact)
+	.use(unexpectedSinon)
 
 export default expect;
--- Website/test/unit/suite/ActionButton.spec.js
+++ Website/test/unit/suite/ActionButton.spec.js
@@ -1,21 +1,41 @@
 import expect from '../helpers/expect.js';
+import sinon from 'sinon';
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import ActionButton from '../../../Composite/console/ActionButton.js';
 
 describe('ActionButton', () => {
-	it('should render a button', () => {
-		var renderer = TestUtils.createRenderer();
-		var props = {
+	let renderer, props, state;
+	beforeEach(() => {
+		renderer = TestUtils.createRenderer();
+		state = { state: true };
+		props = {
 			label: "Label",
-			getState: () => ({state: true }),
-			action: data => ({ data: data })
+			getState: sinon.spy(() => state),
+			action: sinon.spy()
 		}
 		renderer.render(
 			<ActionButton {...props}/>
 		)
+	});
+
+	it('should render a button', () => {
 		return expect(renderer, 'to have rendered',
-			<button>Label</button>
+			<button>{props.label}</button>
 		);
+	});
+
+	it('should get state when clicked', () => {
+		return expect(renderer, 'with event', 'click')
+			.then(() => {
+				return expect(props.getState, 'was called');
+			});
+	});
+
+	it('should call handler with state contents when clicked', () => {
+		return expect(renderer, 'with event', 'click')
+			.then(() => {
+				return expect(props.action, 'was called with', state);
+			});
 	});
 })
