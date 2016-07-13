c05b88d581474595e45cd193ae78d2da0f5316cd
Gert Sønderby <gert.sonderby@gmail.com>
2016-07-13 11:27:06 +0200

    Added test of single-element editor.

--- Website/test/e2e/suite/content/singleEditor.js
+++ Website/test/e2e/suite/content/singleEditor.js
@@ -24,24 +24,45 @@ module.exports = {
 		// Enter frame with editor content
 		editor
 			.enter()
+			// Chack that it is editable
+			.assert.attributeEquals('body', 'contenteditable', 'true')
 			// Check that it has content
 			.section.editorBody
-			.assert.visible('h1');
+			.assert.visible('h1')
+			// Change header
+			.replaceContent('h1', 'Moving forward')
+		// Save change.
+		content
+			.selectFrame('#savebutton')
+			.verify.cssClassNotPresent('#savebutton', 'isdisabled')
+			.click('#savebutton > labelbox');
+		browser
+			.pause(1000)
+		content
+			.verify.cssClassPresent('#savebutton', 'isdisabled')
+			// Close editor after you
+			.enter() // Reset to content frame
+			.section.docktabs.closeTab(2);
+		// Check that the change is made
+		content
+			.enterFrame('@browserFrame')
+			.waitForElementVisible('#browsertabbox iframe', 1000)
+			.waitForFrameLoad('#browsertabbox iframe', 1000)
+			.enterFrame('#browsertabbox iframe')
+			// The below fails if page starts out with unpublished changes.
+			.assert.containsText('div.content-column > h1', 'Moving forward');
 		browser
 			.pause(2000);
-		content
-			.enter()
-			.section.docktabs.closeTab(2);
 	},
 	after: function (browser) {
 		var content = browser.page.content();
 		// Revert changes
-		// content
-			// .enter()
-			// .enterFrame('@browserFrame')
-			// .click('#moreactionsbutton')
-			// .click('menuitem[image="item-undo-unpublished-changes"]')
-			// .click('toolbarbutton[image="item-publish"]');
+		content
+			.enter()
+			.enterFrame('@browserFrame')
+			.click('#moreactionsbutton')
+			.click('menuitem[image="item-undo-unpublished-changes"]')
+			.click('toolbarbutton[image="item-publish"]');
 		browser.end();
 	}
 }
