6093506c4572597fe2e6fec7fd00fc5863799216
Taras Nakonechnyi <neexite@gmail.com>
2016-07-20 17:07:53 +0300

    Fix field help binding

--- Website/Composite/scripts/source/top/ui/bindings/fields/FieldHelpBinding.js
+++ Website/Composite/scripts/source/top/ui/bindings/fields/FieldHelpBinding.js
@@ -7,13 +7,13 @@ FieldHelpBinding.INDICATOR_IMAGE = null;
 /**
  * @class
  */
-function FieldHelpBinding () {
+function FieldHelpBinding() {
 
 	/**
 	 * @type {SystemLogger}
 	 */
-	this.logger = SystemLogger.getLogger ( "FieldHelpBinding" );
-	
+	this.logger = SystemLogger.getLogger("FieldHelpBinding");
+
 	/*
 	 * Returnable.
 	 */
@@ -32,23 +32,23 @@ FieldHelpBinding.prototype.toString = function () {
  * @overloads {Binding#onBindingAttach}
  */
 FieldHelpBinding.prototype.onBindingAttach = function () {
-	
-	FieldHelpBinding.superclass.onBindingAttach.call ( this );
-	
-	this.buildPopupBinding ();
-	this.buildPopupButton ();
+
+	FieldHelpBinding.superclass.onBindingAttach.call(this);
+
+	this.buildPopupBinding();
+	this.buildPopupButton();
 }
 
 /**
  * @overloads {Binding#onBindingDispose}
  */
 FieldHelpBinding.prototype.onBindingDispose = function () {
-	
-	FieldHelpBinding.superclass.onBindingDispose.call ( this );
-	
+
+	FieldHelpBinding.superclass.onBindingDispose.call(this);
+
 	var popup = this._fieldHelpPopupBinding;
-	if ( popup ) {
-		popup.dispose ();
+	if (popup) {
+		popup.dispose();
 	}
 }
 
@@ -56,30 +56,30 @@ FieldHelpBinding.prototype.onBindingDispose = function () {
  * Build popup.
  */
 FieldHelpBinding.prototype.buildPopupBinding = function () {
-	
+
 	var popupSetBinding = app.bindingMap.fieldhelpopupset;
 	var doc = popupSetBinding.bindingDocument;
-	var popupBinding = popupSetBinding.add (
-		PopupBinding.newInstance ( doc )
+	var popupBinding = popupSetBinding.add(
+		PopupBinding.newInstance(doc)
 	);
-	var bodyBinding = popupBinding.add (
-		PopupBodyBinding.newInstance ( doc )
+	var bodyBinding = popupBinding.add(
+		PopupBodyBinding.newInstance(doc)
 	);
 	popupBinding.position = PopupBinding.POSITION_RIGHT;
-	popupBinding.attachClassName ( "fieldhelppopup" );
-	
+	popupBinding.attachClassName("fieldhelppopup");
+
 	/*
 	 * Help can be written inside the tag or supplied in the label attribute.
 	 */
-	if ( this.bindingElement.hasChildNodes ()) {
+	if (this.bindingElement.hasChildNodes()) {
 		bodyBinding.bindingElement.innerHTML = this.bindingElement.innerHTML;
 	} else {
-		var label = this.getProperty ( "label" );
-		if ( label ) {
-			bodyBinding.bindingElement.innerHTML = Resolver.resolve ( label );
+		var label = this.getProperty("label");
+		if (label) {
+			bodyBinding.bindingElement.innerHTML = Resolver.resolve(label);
 		}
 	}
-	
+
 	this.bindingElement.innerHTML = "";
 	this._fieldHelpPopupBinding = popupBinding;
 }
@@ -89,36 +89,35 @@ FieldHelpBinding.prototype.buildPopupBinding = function () {
  */
 FieldHelpBinding.prototype.buildPopupButton = function () {
 
-	var field = this.getAncestorBindingByLocalName ( "field" );
-	
-	if ( field ) {
-	
-		field.attachClassName ( "fieldhelp" );
-		
-		var button = ClickButtonBinding.newInstance ( this.bindingDocument );
-		button.attachClassName ( "fieldhelp" ); 
-	 	button.setImage ( FieldHelpBinding.INDICATOR_IMAGE );
-	 	this.add ( button );
-	 	button.attach ();
-	 	
-	 	var self = this;
-	 	button.oncommand = function () {
-			self.attachPopupBinding ();
-		}
-		
-		button.setPopup ( this._fieldHelpPopupBinding );
-	 	this._fieldHelpButton = button;
+	var field = this.getAncestorBindingByLocalName("field");
+	if (field) {
+		field.attachClassName("fieldhelp");
 	}
+
+	var button = ClickButtonBinding.newInstance(this.bindingDocument);
+	button.attachClassName("fieldhelp");
+	button.setImage(FieldHelpBinding.INDICATOR_IMAGE);
+	this.add(button);
+	button.attach();
+
+	var self = this;
+	button.oncommand = function () {
+		self.attachPopupBinding();
+	}
+
+	button.setPopup(this._fieldHelpPopupBinding);
+	this._fieldHelpButton = button;
+
 }
 
 /**
- * Attach popup. For faster page load time, the popup bindings 
+ * Attach popup. For faster page load time, the popup bindings
  * get attached only when user handles the button.
  */
 FieldHelpBinding.prototype.attachPopupBinding = function () {
 
 	var popup = this._fieldHelpPopupBinding;
-	if ( popup && !popup.isAttached ) {
-		popup.attachRecursive ();
+	if (popup && !popup.isAttached) {
+		popup.attachRecursive();
 	}
 }
