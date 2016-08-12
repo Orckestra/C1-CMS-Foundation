76572854589c1e6195cf29ecf7da9ce7aaafd9f4
Gert Sønderby <gert.sonderby@gmail.com>
2016-08-12 16:23:28 +0200

    Cleanup, separate coverage generation into own script.

---
+++ Website/test/unit/coverage.js
@@ -0,0 +1,18 @@
+/* global SystemJS */
+import systemIstanbul from '@node/systemjs-istanbul-hook';
+systemIstanbul.hookSystemJS(SystemJS, address => {
+	// Only files in Composite/console should be instrumented
+	return !address.startsWith(SystemJS.baseURL + 'Composite/console');
+});
+import fs from '@node/fs';
+import runner from './test/unit/runner';
+
+runner
+// after running the tests, save the coverage file
+.then(function() {
+	fs.writeFileSync('coverage.json', JSON.stringify(systemIstanbul.remapCoverage()));
+})
+.catch(err => {
+	console.error(err); // eslint-disable-line no-console
+	console.error(err.stack); // eslint-disable-line no-console
+});
--- Website/test/unit/runner.js
+++ Website/test/unit/runner.js
@@ -1,18 +1,10 @@
 /* global SystemJS */
-import sourceMapSupport from '@node/source-map-support';
-sourceMapSupport.install({ environment: 'node' });
 import Mocha from '@node/mocha';
-import systemIstanbul from '@node/systemjs-istanbul-hook';
-systemIstanbul.hookSystemJS(SystemJS, address => {
-	// Only files in Composite/console should be instrumented
-	return !address.startsWith(SystemJS.baseURL + 'Composite/console');
-});
-import fs from '@node/fs';
 
 var runner = new Mocha({ ui: 'bdd' });
 runner.suite.emit('pre-require', global, 'global-mocha-context', runner);
 
-SystemJS.import('./test/unit/suite.js').then(i => i.importSuite())
+export default SystemJS.import('./test/unit/suite.js').then(i => i.importSuite())
 .then(function() {
 	return new Promise((resolve, reject) => {
 		runner.run((failures) => {
@@ -23,10 +15,6 @@ SystemJS.import('./test/unit/suite.js').then(i => i.importSuite())
 			}
 		});
 	});
-})
-// after running the tests, save the coverage file
-.then(function() {
-	fs.writeFileSync('coverage.json', JSON.stringify(systemIstanbul.remapCoverage()));
 })
 .catch(err => {
 	console.error(err); // eslint-disable-line no-console
