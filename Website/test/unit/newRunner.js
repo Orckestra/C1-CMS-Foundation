d87017f206e807e0c2ebb88505c6803c0517eb2b
Gert Sønderby <gert.sonderby@gmail.com>
2016-08-12 15:22:28 +0200

    Istanbul instrumentation now works.

--- Website/test/unit/newRunner.js
+++ Website/test/unit/newRunner.js
@@ -2,9 +2,12 @@
 import sourceMapSupport from '@node/source-map-support';
 sourceMapSupport.install({ environment: 'node' });
 import Mocha from '@node/mocha';
-// import systemIstanbul from '@node/systemjs-istanbul-hook';
-// systemIstanbul.hookSystemJS(SystemJS);
-// import fs from '@node/fs';
+import systemIstanbul from '@node/systemjs-istanbul-hook';
+systemIstanbul.hookSystemJS(SystemJS, address => {
+	// Only files in Composite/console should be instrumented
+	return !address.startsWith(SystemJS.baseURL + 'Composite/console');
+});
+import fs from '@node/fs';
 
 var runner = new Mocha({ ui: 'bdd' });
 runner.suite.emit('pre-require', global, 'global-mocha-context', runner);
@@ -22,7 +25,10 @@ SystemJS.import('./test/unit/suite.js').then(i => i.importSuite())
 	});
 })
 // after running the tests, save the coverage file
-// .then(function() {
-// 	fs.writeFileSync('coverage.json', JSON.stringify(systemIstanbul.remapCoverage()));
-// })
-.catch(console.error.bind(console)); // eslint-disable-line no-console
+.then(function() {
+	fs.writeFileSync('coverage.json', JSON.stringify(systemIstanbul.remapCoverage()));
+})
+.catch(err => {
+	console.error(err); // eslint-disable-line no-console
+	console.error(err.stack); // eslint-disable-line no-console
+});
