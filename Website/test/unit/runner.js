e7ba0665810ab20d57b56667df420d29f75b0cfd
Gert Sønderby <gert.sonderby@gmail.com>
2016-08-12 16:08:13 +0200

    Replaced old test runner.

--- Website/test/unit/runner.js
+++ Website/test/unit/runner.js
@@ -1,23 +1,34 @@
-// Set up and run the test suite
-import glob from 'glob';
+/* global SystemJS */
+import sourceMapSupport from '@node/source-map-support';
+sourceMapSupport.install({ environment: 'node' });
+import Mocha from '@node/mocha';
+import systemIstanbul from '@node/systemjs-istanbul-hook';
+systemIstanbul.hookSystemJS(SystemJS, address => {
+	// Only files in Composite/console should be instrumented
+	return !address.startsWith(SystemJS.baseURL + 'Composite/console');
+});
+import fs from '@node/fs';
 
-const jsFileEx = /.js$/;
-function describeDirs(testDir) {
-	let cwd = './test/unit/suite' + (testDir ? '/' + testDir : '');
-	let dirs = glob.sync('*', { cwd });
-	dirs.forEach(pathName => {
-		let fullPathName = (testDir ? testDir + '/' : '') + pathName;
-		if (jsFileEx.test(pathName)) {
-			// Test file, require in where we stand
-			require('./suite/' + fullPathName);
-		} else {
-			// Directory, describe by name, move in and check contents
-			let groupName = pathName[0].toUpperCase() + pathName.slice(1);
-			describe(groupName, () => {
-				describeDirs(fullPathName);
-			});
-		}
+var runner = new Mocha({ ui: 'bdd' });
+runner.suite.emit('pre-require', global, 'global-mocha-context', runner);
+
+SystemJS.import('./test/unit/suite.js').then(i => i.importSuite())
+.then(function() {
+	return new Promise((resolve, reject) => {
+		runner.run((failures) => {
+			if (failures) {
+				reject(failures);
+			} else {
+				resolve();
+			}
+		});
 	});
-
-}
-describeDirs();
+})
+// after running the tests, save the coverage file
+.then(function() {
+	fs.writeFileSync('coverage.json', JSON.stringify(systemIstanbul.remapCoverage()));
+})
+.catch(err => {
+	console.error(err); // eslint-disable-line no-console
+	console.error(err.stack); // eslint-disable-line no-console
+});
