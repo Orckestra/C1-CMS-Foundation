007c1371f7f79e3e0944a2034fd3efce90291be1
Gert Sønderby <gert.sonderby@gmail.com>
2016-08-15 09:21:37 +0200

    Reinstated test grepping

--- Website/test/unit/runner.js
+++ Website/test/unit/runner.js
@@ -4,6 +4,12 @@ import Mocha from '@node/mocha';
 var runner = new Mocha({ ui: 'bdd' });
 runner.suite.emit('pre-require', global, 'global-mocha-context', runner);
 
+// Slightly hacky grep switch
+var grepSwitchIndex = process.argv.indexOf('-g');
+if (grepSwitchIndex > -1) {
+	runner.grep(process.argv[grepSwitchIndex + 1] || '');
+}
+
 export default SystemJS.import('./test/unit/suite.js').then(i => i.importSuite())
 .then(function() {
 	return new Promise((resolve, reject) => {
