adc9c1c802c32a6c21f319fe1bebb26e0286e739
Gert Sønderby <gert.sonderby@gmail.com>
2016-08-16 09:47:36 +0200

    Re-enabled path-based nesting suites.

--- Website/test/unit/runner.js
+++ Website/test/unit/runner.js
@@ -1,7 +1,9 @@
 /* global SystemJS */
 import Mocha from '@node/mocha';
+import bdd from '/test/unit/helpers/ui.js';
+Mocha.interfaces['bdd-openstack'] = bdd;
 
-var runner = new Mocha({ ui: 'bdd' });
+var runner = new Mocha({ ui: 'bdd-openstack' });
 runner.suite.emit('pre-require', global, 'global-mocha-context', runner);
 
 // Slightly hacky grep switch
@@ -10,7 +12,8 @@ if (grepSwitchIndex > -1) {
 	runner.grep(process.argv[grepSwitchIndex + 1] || '');
 }
 
-export default SystemJS.import('./test/unit/suite.js').then(i => i.importSuite())
+export default SystemJS.import('./test/unit/suite.js')
+.then(i => i.importSuite())
 .then(function() {
 	return new Promise((resolve, reject) => {
 		runner.run((failures) => {
--- Website/test/unit/suite.js
+++ Website/test/unit/suite.js
@@ -1,7 +1,16 @@
-/* global SystemJS */
+/* global SystemJS, suiteStack */
 import glob from 'glob';
+import { Suite } from '@node/mocha';
+
 export function importSuite() {
 	return describeDirs();
+}
+
+function forEachPromise(items, iterator) {
+	// Iterate through array, chain promises up for each element.
+	// Iterator shopuld return a promise if it is async-dependent
+	var p = Promise.resolve(null);
+	return items.reduce((promise, item) => promise.then(() => iterator(item)), p);
 }
 
 const baseDir = './test/unit/suite';
@@ -9,17 +18,19 @@ const jsFileEx = /.js$/;
 function describeDirs(testDir) {
 	let cwd = baseDir + (testDir ? '/' + testDir : '');
 	let dirs = glob.sync('*', { cwd });
-	return Promise.all(dirs.map(pathName => {
+	return forEachPromise(dirs, pathName => {
 		let fullPathName = (testDir ? testDir + '/' : '') + pathName;
 		if (jsFileEx.test(pathName)) {
-			// Test file, require in where we stand
+			// Test file, import where we stand
 			return SystemJS.import(baseDir + '/' + fullPathName);
 		} else {
 			// Directory, describe by name, move in and check contents
-			// let groupName = pathName[0].toUpperCase() + pathName.slice(1);
-			// describe(groupName, () => {
-			return describeDirs(fullPathName);
-			// });
+			let groupName = pathName[0].toUpperCase() + pathName.slice(1);
+			let newSuite = Suite.create(suiteStack[0], groupName);
+			newSuite.file = 'global-mocha-context';
+			suiteStack.unshift(newSuite);
+			return describeDirs(fullPathName)
+			.then(() => suiteStack.shift());
 		}
-	}));
+	});
 }
