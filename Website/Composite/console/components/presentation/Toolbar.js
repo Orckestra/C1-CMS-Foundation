76adb8356805d741d4123ff91d0848d58a349138
Gert Sønderby <gert.sonderby@gmail.com>
2016-09-28 11:07:34 +0200

    Added basic support for selectors in toolbars

--- Website/Composite/console/components/presentation/Toolbar.js
+++ Website/Composite/console/components/presentation/Toolbar.js
@@ -1,17 +1,28 @@
 import React, { PropTypes } from 'react';
 import ActionButton from 'console/components/presentation/ActionButton.js';
+import Select from 'react-select';
 
 const Toolbar = ({ style, items, canSave }) => (
 	<div className={'toolbar' + (style ? ' ' + style : '')}>
 		{Object.keys(items).map(name => {
-			let button = Object.assign({}, items[name]);
-			if (!((button.label || button.icon) && button.action)) return null;
-			button.disabled = button.saveButton && !canSave;
-			delete button.saveButton;
-			return <ActionButton
-				key={name}
-				{...button}/>;
-		}).filter(button => !!button)}
+			let item = Object.assign({}, items[name]);
+			switch (item.type) {
+			case 'select':
+				item.options = item.options.map(option => ({
+					value: option.value,
+					label: option.label || option.value
+				}));
+				return <Select key="name" {...item}/>;
+			case 'button':
+			default:
+				if (!((item.label || item.icon) && item.action)) return null;
+				item.disabled = item.saveButton && !canSave;
+				delete item.saveButton;
+				return <ActionButton
+					key={name}
+					{...item}/>;
+			}
+		}).filter(item => !!item)}
 	</div>
 );
 
--- Website/test/unit/suite/view/presentation/Toolbar.spec.js
+++ Website/test/unit/suite/view/presentation/Toolbar.spec.js
@@ -3,6 +3,7 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import Toolbar from 'console/components/presentation/Toolbar.js';
 import ActionButton from 'console/components/presentation/ActionButton.js';
+import Select from 'react-select';
 
 describe('Toolbar', () => {
 	let renderer, props, actions;
@@ -16,7 +17,13 @@ describe('Toolbar', () => {
 		props = {
 			name: 'toolbar',
 			canSave: false,
-			items: {
+			items: {}
+		};
+	});
+
+	describe('with buttons', () => {
+		beforeEach(() => {
+			props.items = {
 				first: {
 					type: 'button',
 					label: 'Label1',
@@ -34,42 +41,73 @@ describe('Toolbar', () => {
 					saveButton: true,
 					action: actions.third
 				}
-			}
-		};
+			};
+		});
+
+		it('renders them', () => {
+			renderer.render(<Toolbar {...props}/>);
+			return expect(renderer, 'to have rendered',
+				<div className='toolbar'>
+					<ActionButton label='Label1' action={actions.first} icon='save'/>
+					<ActionButton label='Label2' action={actions.second}/>
+					<ActionButton label='Label3' action={actions.third} disabled={true}/>
+				</div>
+			);
+		});
+
+		it('does not render them where action is missing', () => {
+			delete props.items.third.action;
+			renderer.render(<Toolbar {...props}/>);
+			return expect(renderer, 'to have rendered',
+				<div className='toolbar'>
+					<ActionButton label='Label1' action={actions.first} icon='save'/>
+					<ActionButton label='Label2' action={actions.second}/>
+				</div>
+			);
+		});
+
+		it('renders them where label is missing, but only if it has icon', () => {
+			delete props.items.first.label;
+			delete props.items.third.label;
+			renderer.render(<Toolbar {...props}/>);
+			return expect(renderer, 'to have rendered',
+				<div className='toolbar'>
+					<ActionButton action={actions.first} icon='save'/>
+					<ActionButton label='Label2' action={actions.second}/>
+				</div>
+			);
+		});
 	});
 
-	it('renders buttons', () => {
-		renderer.render(<Toolbar {...props}/>);
-		return expect(renderer, 'to have rendered',
-			<div className='toolbar'>
-				<ActionButton label='Label1' action={actions.first} icon='save'/>
-				<ActionButton label='Label2' action={actions.second}/>
-				<ActionButton label='Label3' action={actions.third} disabled={true}/>
-			</div>
-		);
-	});
+	describe('with selector', () => {
+		beforeEach(() => {
+			props.items = {
+				first: {
+					type: 'select',
+					placeholder: 'Select date',
+					options: [
+						{ value: '2016-09-23' },
+						{ value: '2016-09-22' },
+						{ value: '2016-09-21', label: 'That day' },
+						{ value: '2016-09-20' }
+					]
+				}
+			};
+		});
 
-	it('does not render buttons where action is missing', () => {
-		delete props.items.third.action;
-		renderer.render(<Toolbar {...props}/>);
-		return expect(renderer, 'to have rendered',
-			<div className='toolbar'>
-				<ActionButton label='Label1' action={actions.first} icon='save'/>
-				<ActionButton label='Label2' action={actions.second}/>
-			</div>
-		);
-	});
-
-	it('renders buttons where label is missing, but only if it has icon', () => {
-		delete props.items.first.label;
-		delete props.items.third.label;
-		renderer.render(<Toolbar {...props}/>);
-		return expect(renderer, 'to have rendered',
-			<div className='toolbar'>
-				<ActionButton action={actions.first} icon='save'/>
-				<ActionButton label='Label2' action={actions.second}/>
-			</div>
-		);
+		it('renders them', () => {
+			renderer.render(<Toolbar {...props}/>);
+			return expect(renderer, 'to have rendered',
+				<div className='toolbar'>
+					<Select placeholder='Select date' options={[
+						{ value: '2016-09-23', label: '2016-09-23' },
+						{ value: '2016-09-22', label: '2016-09-22' },
+						{ value: '2016-09-21', label: 'That day' },
+						{ value: '2016-09-20', label: '2016-09-20' }
+					]}/>
+				</div>
+			);
+		});
 	});
 
 	describe('styles', () => {
@@ -77,11 +115,7 @@ describe('Toolbar', () => {
 			props.style = 'light rightAligned';
 			renderer.render(<Toolbar {...props}/>);
 			return expect(renderer, 'to have rendered',
-				<div className='toolbar light rightAligned'>
-					<ActionButton label='Label1' action={actions.first} icon='save'/>
-					<ActionButton label='Label2' action={actions.second}/>
-					<ActionButton label='Label3' action={actions.third} disabled={true}/>
-				</div>
+				<div className='toolbar light rightAligned'/>
 			);
 		});
 	});
