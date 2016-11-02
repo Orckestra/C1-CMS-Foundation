79691b184e6035a4c979f5370481ebf762e662a2
Vitalii Vysotskyi <vitaly.vysotskyi@orckestra.com>
2016-11-02 11:13:14 +0200

    Website Installation (Other Language)
    1. Test: 000_setup/installSite.js updated: uses installWebsite.js
    command now
    2.  Test: locales/InstallSiteWithOtherLanguage.js finalized: uses French
    (CA) and asserts the GUI and the locale are changed accordingly; rolls
    back the website back to English (US)English

--- Website/test/e2e/suite/locales/InstallSiteWithOtherLanguage.js
+++ Website/test/e2e/suite/locales/InstallSiteWithOtherLanguage.js
@@ -2,23 +2,42 @@ var resetSite = require('../../reset.js');
 
 module.exports = {
 	'@tags': ['install'],
-	before: function (browser, done) {
+	beforeEach: function (browser) {
+	// 0 Reset the website
 		resetSite(function (err) {
 			if (err) {
 				browser.end();
 				console.error(err);
 				process.exit(1);
 			}
-			done();
 		});
-	},
-	beforeEach: function (browser) {
     // 1  Launch an uninitialized website.
 		browser
       .url(browser.launchUrl + '/Composite/top.aspx')
       .waitForElementVisible('.welcomepage', browser.globals.timeouts.basic);
 	},
-	'install Venus starter site': function (browser) {
-		browser.installWebsite('en-US', 'French (CA)', 'fr-CA')
+	
+	'install Venus starter site with French (CA)': function (browser) {
+		browser
+		.installWebsite('en-US', 'French (CA)', 'fr-CA')
+		
+		// assert the GUI and the website language is French 
+		browser
+		.selectPerspective("Système")
+		.openTreeNode("Langues")
+		.assertTreeNodeHasChild("Langues", "Français, Canada")
+		
+	},
+	
+	'reinstall Venus starter site with English (US)': function (browser) {
+		browser.installWebsite('en-US')
+		
+		// assert the GUI and the website language is English 
+		browser
+		.selectPerspective("System")
+		.openTreeNode("Languages")
+		.assertTreeNodeHasChild("Languages", "English, US")
 	}
+	
+	
 }
