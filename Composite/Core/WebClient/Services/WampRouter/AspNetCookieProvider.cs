8d49395e4be21cb6396b09204045d85a3bcf009a
Morteza Kasravi <kasravi@gmail.com>
2016-11-15 15:45:12 +0100

    Wamp Router Initial Commit

---
+++ Composite/Core/WebClient/Services/WampRouter/AspNetCookieProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Web;
+using WampSharp.V2.Authentication;
+
+namespace Composite.Core.WebClient.Services.WampRouter
+{
+    internal class AspNetCookieProvider : CookieCollectionCookieProvider
+    {
+        public AspNetCookieProvider(HttpContext httpContext) :
+            base(GetCookieCollection(httpContext))
+        {
+        }
+
+        private static CookieCollection GetCookieCollection(HttpContext httpContext)
+        {
+            CookieCollection result = new CookieCollection();
+
+            foreach (KeyValuePair<string, string> cookie in httpContext.Request.Cookies)
+            {
+                result.Add(new Cookie(cookie.Key, cookie.Value));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Composite/Core/WebClient/Services/WampRouter/WampRouter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Reactive.Subjects;
+using WampSharp.Binding;
+using WampSharp.V2;
+using WampSharp.V2.Realm;
+
+namespace Composite.Core.WebClient.Services.WampRouter
+{
+    internal class WampRouter
+    {
+        private const string DefaultRealmName = "realm1";
+        private WampHost _host;
+
+        public WampRouter()
+        {
+            StartWampRouter();
+        }
+
+        public void RegisterCallee(string realmName, IRpcService instance) 
+        {
+            var realm = _host.RealmContainer.GetRealmByName(realmName);
+
+            var registrationTask = realm.Services.RegisterCallee(instance);
+            registrationTask.Wait();
+        }
+
+        public void RegisterPublisher<T>(string realmName, string topicName, IWampEventHandler<T> eventObservable)
+        {
+            IWampHostedRealm realm = _host.RealmContainer.GetRealmByName(realmName);
+
+            ISubject<T> subject =
+                realm.Services.GetSubject<T>(topicName);
+
+            IObservable<long> observableEvent = eventObservable.Event;
+
+            IDisposable disposable =
+                observableEvent.Subscribe(x =>
+                {
+                    try
+                    {
+                        subject.OnNext(eventObservable.GetNewData());
+                    }
+                    catch (Exception)
+                    {
+                        //TODO: Why it publishes data and generates error
+                    }
+                });
+        }
+
+        private void StartWampRouter()
+        {
+            _host = new WampHost();
+            _host.RegisterTransport(new AspNetWebsocketTransform(),
+                new JTokenJsonBinding());
+            IWampHostedRealm realm = _host.RealmContainer.GetRealmByName(DefaultRealmName);
+
+            realm.SessionCreated += SessionCreated;
+            realm.SessionClosed += SessionRemoved;
+            _host.Open();
+        }
+        private static void SessionCreated(object sender, WampSessionCreatedEventArgs e)
+        {
+            Log.LogVerbose(nameof(WampRouter),"A new wamp client is connected");
+        }
+
+        private static void SessionRemoved(object sender, WampSessionCloseEventArgs e)
+        {
+            Log.LogVerbose(nameof(WampRouter), "A connection error occured");
+        }
+    }
+}
+
