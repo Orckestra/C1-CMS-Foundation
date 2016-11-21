404cb7e53527f8fda42f8d7b059c66c8821bc7cb
Marcus Wendt <marcus@wendt.dk>
2016-11-21 12:29:54 +0100

    Reordering using statements.

--- Composite/Core/ServiceLocator.cs
+++ Composite/Core/ServiceLocator.cs
@@ -1,9 +1,9 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.Extensions.DependencyInjection;
-using System.Collections.Generic;
-using System.Collections.Concurrent;
 
 namespace Composite.Core
 {
