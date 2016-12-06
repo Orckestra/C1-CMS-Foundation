2910d016f40759fa77c55ad2cdf94f972a4e4c67
Morteza Kasravi <kasravi@gmail.com>
2016-12-06 16:53:09 +0100

    Container Classes on Components

--- Composite/C1Console/RichContent/Components/Component.cs
+++ Composite/C1Console/RichContent/Components/Component.cs
@@ -12,6 +12,7 @@ namespace Composite.C1Console.RichContent.Components
         public virtual string Title { get; set; }
         public virtual string Description { get; set; }
         public virtual IEnumerable<string> GroupingTags { get; set; }
+        public virtual IEnumerable<string> ContainerClasses { get; set; }
         public virtual XDocument ComponentDefinition { get; set; }
     }
 }
