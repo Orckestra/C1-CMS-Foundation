f8f85a2c893cf06fb708a288fe488895a078711e
Gert Sønderby <gert.sonderby@gmail.com>
2016-12-09 13:45:18 +0100

    Added a number of extra components to mock list, in order to test dialog layout

--- Website/Composite/console/mocks/services/functionMock.js
+++ Website/Composite/console/mocks/services/functionMock.js
@@ -14,6 +14,30 @@ const functionList = [
 				label: 'Video',
 				description: 'Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.',
 				previewImageUrl: '/media/393eeb79-ec4b-4a36-ab03-1b71e17112e2/vENN5g/Composite.Community.Profiles/Profiles-2.jpg?h=100'
+			},
+			{
+				name: 'music-insert',
+				label: 'Music',
+				description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.',
+				previewImageUrl: '/media/b72125aa-5828-463b-8548-f43d65ee33e0/-Rj7RQ/Copenhagen/Vikings__Photographer_Frederikssund_Turistbureau.jpg?w=100'
+			},
+			{
+				name: 'banana-insert',
+				label: 'Banana',
+				description: 'Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.',
+				previewImageUrl: '/media/393eeb79-ec4b-4a36-ab03-1b71e17112e2/vENN5g/Composite.Community.Profiles/Profiles-2.jpg?h=100'
+			},
+			{
+				name: 'random-insert',
+				label: 'Random',
+				description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.',
+				previewImageUrl: '/media/b72125aa-5828-463b-8548-f43d65ee33e0/-Rj7RQ/Copenhagen/Vikings__Photographer_Frederikssund_Turistbureau.jpg?w=100'
+			},
+			{
+				name: 'absurd-insert',
+				label: 'Absurdity',
+				description: 'Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.',
+				previewImageUrl: '/media/393eeb79-ec4b-4a36-ab03-1b71e17112e2/vENN5g/Composite.Community.Profiles/Profiles-2.jpg?h=100'
 			}
 		]
 	},
