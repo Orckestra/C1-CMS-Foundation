52bf0e59ad0ad36ab66cc68dbb3653dd3e41b7cd
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2017-01-02 14:49:35 +0100

    Search - cleaning up old filtering code

--- Composite/C1Console/Search/SearchFacade.cs
+++ Composite/C1Console/Search/SearchFacade.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Composite.C1Console.Search.DocumentSources;
-using Composite.C1Console.Security;
 using Composite.Core;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,8 +12,6 @@ namespace Composite.C1Console.Search
     /// </summary>
     public static class SearchFacade
     {
-        const int AncestorCheckLevelsDeep = 20;
-
         /// <summary>
         /// Gets the document sources
         /// </summary>
@@ -38,93 +34,16 @@ namespace Composite.C1Console.Search
         /// Executes a search query
         /// </summary>
         /// <param name="query"></param>
-        /// <param name="applySecurity"></param>
-        /// <param name="searchRoot"></param>
         /// <returns></returns>
         public static async Task<SearchResult> SearchConsoleAsync(
-            SearchQuery query, 
-            bool applySecurity = true,
-            EntityToken searchRoot = null)
+            SearchQuery query)
         {
             if (!SearchEnabled)
             {
                 return SearchResult.Empty;
             }
 
-            var result = await SearchFacade.SearchProvider.SearchAsync(query);
-
-            var documents = result.Documents;
-            if (applySecurity || searchRoot != null)
-            {
-                return new SearchResult
-                {
-                    Documents = Filter(documents, applySecurity, searchRoot),
-                    Facets = result.Facets,
-                    TotalHits = result.TotalHits
-                };
-            }
-
-            return result;
-        }
-
-        internal static IEnumerable<SearchDocument> Filter(
-            IEnumerable<SearchDocument> documents,
-            bool applySecurity,
-            EntityToken searchRoot)
-        {
-            var userSecurityData = applySecurity ? new CurrentUserSecurityData() : null;
-
-            foreach (var doc in documents)
-            {
-                EntityToken entityToken;
-                try
-                {
-                    entityToken = EntityTokenSerializer.Deserialize(doc.SerializedEntityToken);
-                }
-                catch (Exception ex)
-                {
-                    // TODO: collect documents with invalid entity tokens
-                    Log.LogWarning(nameof(SearchFacade), ex);
-                    continue;
-                }
-
-                if (userSecurityData != null && !userSecurityData.UserHasAccessTo(entityToken))
-                {
-                    continue;
-                }
-
-                if (searchRoot != null && !EntityTokenHasAncestor(entityToken, searchRoot))
-                {
-                    continue;
-                }
-
-                yield return doc;
-            }
-        }
-
-        private static bool EntityTokenHasAncestor(EntityToken entityToken, EntityToken ancestorEntityToken)
-        {
-            return ParentsFacade.GetAllParents(entityToken, AncestorCheckLevelsDeep).Contains(ancestorEntityToken);
-        }
-
-        private class CurrentUserSecurityData
-        {
-            private readonly UserToken _userToken;
-            private readonly IEnumerable<UserPermissionDefinition> _userPermissionDefinitions;
-            private readonly IEnumerable<UserGroupPermissionDefinition> _userGroupPermissionDefinition;
-
-            public CurrentUserSecurityData()
-            {
-                _userToken = UserValidationFacade.GetUserToken();
-                _userPermissionDefinitions = PermissionTypeFacade.GetUserPermissionDefinitions(_userToken.Username);
-                _userGroupPermissionDefinition = PermissionTypeFacade.GetUserGroupPermissionDefinitions(_userToken.Username);
-            }
-
-            public bool UserHasAccessTo(EntityToken entityToken)
-            {
-                return PermissionTypeFacade.IsSubBrachContainingPermissionTypes(
-                    _userToken, entityToken, _userPermissionDefinitions, _userGroupPermissionDefinition);
-            }
+            return await SearchProvider.SearchAsync(query);
         }
 
         internal static void AddDefaultSearchDocumentSourceProviders(this IServiceCollection services)
