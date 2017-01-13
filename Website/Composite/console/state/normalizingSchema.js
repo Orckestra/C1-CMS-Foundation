44f01fb211997901fa9751d2f98e08de2c5d9fcf
Gert Sønderby <gert.sonderby@gmail.com>
2017-01-13 14:48:44 +0100

    Minor dependency update.

--- Website/Composite/console/state/normalizingSchema.js
+++ Website/Composite/console/state/normalizingSchema.js
@@ -1,20 +1,26 @@
 import { Schema, arrayOf } from 'normalizr';
 
 export const dataFieldSchema = new Schema('dataFieldDefs', { idAttribute: 'name' });
+
 export const fieldsetSchema = new Schema('fieldsetDefs', { idAttribute: 'name' });
 fieldsetSchema.define({
 	fields: arrayOf(dataFieldSchema)
 });
+
 export const tabSchema = new Schema('tabDefs', { idAttribute: 'name' });
 tabSchema.define({
 	fieldsets: arrayOf(fieldsetSchema),
 });
+
 export const itemSchema = new Schema('itemDefs', { idAttribute: 'name' });
+
 export const toolbarSchema = new Schema('toolbarDefs', { idAttribute: 'name' });
 toolbarSchema.define({
 	items: arrayOf(itemSchema)
 });
+
 export const providerSchema = new Schema('providerDefs', { idAttribute: 'name' });
+
 export const dialogPaneSchema = new Schema('dialogPaneDefs', { idAttribute: 'name' });
 dialogPaneSchema.define({
 	provider: providerSchema,
@@ -22,10 +28,12 @@ dialogPaneSchema.define({
 	cancelProvider: providerSchema,
 	finishProvider: providerSchema
 });
+
 export const dialogSchema = new Schema('dialogDefs', { idAttribute: 'name' });
 dialogSchema.define({
 	panes: arrayOf(dialogPaneSchema)
 });
+
 export const pageSchema = new Schema('pageDefs', { idAttribute: 'name' });
 pageSchema.define({
 	tabs: arrayOf(tabSchema),
