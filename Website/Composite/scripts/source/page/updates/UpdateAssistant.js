e3d7901e5242a374351050977042ca698b1f3b32
neexite <neexite@gmail.com>
2017-04-11 15:45:05 +0300

    Fix #427 [Console] User input can be reverted by the UpdateManager.js when there's a slow internet connection and multiple form elements

--- Website/Composite/scripts/source/page/updates/UpdateAssistant.js
+++ Website/Composite/scripts/source/page/updates/UpdateAssistant.js
@@ -154,9 +154,11 @@ _UpdateAssistant.prototype = {
 		var request = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Msxml2.XMLHTTP.3.0");
 
 		if (request != null) {
+			Application.lock(target);
 			request.open(method, target, (handler != null ? true : false));
 			if (handler != null) {
 				function action() {
+					Application.unlock(target);
 					if (request.readyState == 4) {
 						var errorType = request.getResponseHeader("X-Error-Type");
 						// Handle error
