02589b7760ccf886ea86f56527e7185293420d67
Marcus Wendt <marcus@wendt.dk>
2017-07-24 16:54:50 +0200

    more robust dev tool reset-installation.bat

--- Website/App_Data/Composite/reset-installation.bat
+++ Website/App_Data/Composite/reset-installation.bat
@@ -1,4 +1,5 @@
-﻿del DataMetaData\*.xml
+
+del DataMetaData\*.xml
 del DataStores\*.xml
 del Configuration\DynamicSqlDataProvider.config
 del Configuration\DynamicXmlDataProvider.config
@@ -31,7 +32,6 @@ md ApplicationState\SerializedWorkflows
 rd ..\..\Composite\InstalledPackages /S /Q
 rd ..\..\Views /S /Q
 rd Azure /S /Q
-md Azure
 rd InlineCSharpFunctions /S /Q
 md InlineCSharpFunctions
 
@@ -50,28 +50,28 @@ rd ..\..\App_Data\PageTemplateFeatures /S /Q
 
 :: Razor cleanup
 copy ..\..\App_Data\Razor\web.config ..\..\App_Data\Razor.web.config
-rd ..\..\App_Data\Razor /S /Q
-md ..\..\App_Data\Razor
+del /q ..\..\App_Data\Razor
+FOR /D %%p IN (..\..\App_Data\Razor\*.*) DO rmdir "%%p" /s /q
 copy ..\..\App_Data\Razor.web.config ..\..\App_Data\Razor\web.config
 del ..\..\App_Data\Razor.web.config /f
 
 :: UserControls cleanup
 copy ..\..\App_Data\UserControls\web.config ..\..\App_Data\UserControls.web.config
-rd ..\..\App_Data\UserControls /S /Q
-md ..\..\App_Data\UserControls
+del /q ..\..\App_Data\UserControls
+FOR /D %%p IN (..\..\App_Data\UserControls\*.*) DO rmdir "%%p" /s /q
 copy ..\..\App_Data\UserControls.web.config ..\..\App_Data\UserControls\web.config
 del ..\..\App_Data\UserControls.web.config /f
 
 
 :: PageTemplates cleanup
 copy ..\..\App_Data\PageTemplates\web.config ..\..\App_Data\PageTemplates.web.config
-rd ..\..\App_Data\PageTemplates /S /Q
-md ..\..\App_Data\PageTemplates
+del /q ..\..\App_Data\PageTemplates
+FOR /D %%p IN (..\..\App_Data\PageTemplates\*.*) DO rmdir "%%p" /s /q
 copy ..\..\App_Data\PageTemplates.web.config ..\..\App_Data\PageTemplates\web.config
 del ..\..\App_Data\PageTemplates.web.config /f
 
 :: License file cleanup
-del PackageLicenses\*.* /y
+del PackageLicenses\*.* /q
 
 :: Assembly cleanup
 rmdir ..\..\bin /S /Q
