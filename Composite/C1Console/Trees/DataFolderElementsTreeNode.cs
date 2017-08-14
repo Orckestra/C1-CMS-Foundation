073064628916162b8136d6cdd26c3a74b81f78db
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2017-08-14 15:39:45 +0200

    Fixing an exception when getting children for a tree element that doesn't exist any more

--- Composite/C1Console/Trees/DataFolderElementsTreeNode.cs
+++ Composite/C1Console/Trees/DataFolderElementsTreeNode.cs
@@ -193,7 +193,7 @@ namespace Composite.C1Console.Trees
                     if (dataEntityToken != null)
                     {
                         var data = dataEntityToken.Data;
-                        Verify.IsNotNull(data, "data is null, " + dataEntityToken);
+                        if (data == null) return Enumerable.Empty<Element>();
 
                         referenceType = this.ChildGeneratingParentIdFilterNode.KeyPropertyInfo.DeclaringType;
                         referenceValue = this.ChildGeneratingParentIdFilterNode.KeyPropertyInfo.GetValue(data, null);
