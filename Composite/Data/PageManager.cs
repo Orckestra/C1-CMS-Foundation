00010ee663863728ca80a4c7ec8db6523c76487a
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2017-09-29 17:38:33 +0200

    Increasing the max size and mentioning to the config files for the "Page placeholders" cache; refactoring

--- Composite/Data/PageManager.cs
+++ Composite/Data/PageManager.cs
@@ -6,7 +6,6 @@ using Composite.Core;
 using Composite.Core.Linq;
 using Composite.Data.Caching;
 using Composite.Data.Foundation;
-using Composite.Core.Extensions;
 using Composite.Core.Types;
 using Composite.Data.Types;
 
@@ -39,7 +38,7 @@ namespace Composite.Data
         private static readonly int PageCacheSize = 5000;
         private static readonly int PageStructureCacheSize = 3000;
         private static readonly int ChildrenCacheSize = 2000;
-        private static readonly int PagePlaceholderCacheSize = 500;
+        private static readonly int PagePlaceholderCacheSize = 10000;
 
         private static readonly Cache<string, IReadOnlyCollection<IPage>> _pageCache = new Cache<string, IReadOnlyCollection<IPage>>("Pages", PageCacheSize);
         private static readonly Cache<string, ReadOnlyCollection<IPagePlaceholderContent>> _placeholderCache = new Cache<string, ReadOnlyCollection<IPagePlaceholderContent>>("Page placeholders", PagePlaceholderCacheSize);
@@ -301,7 +300,7 @@ namespace Composite.Data
 
             if (result == null)
             {
-                Log.LogWarning(LogTitle, "No IPageStructure entries found for Page with Id '{0}'".FormatWith(pageId));
+                Log.LogWarning(LogTitle, $"No IPageStructure entries found for Page with Id '{pageId}'");
             }
 
             _pageStructureCache.Add(cacheKey, result);
@@ -348,13 +347,10 @@ namespace Composite.Data
 
         private static void OnPageChanged(object sender, DataEventArgs args)
         {
-            IPage page = args.Data as IPage;
-            if (page == null)
+            if (args.Data is IPage page)
             {
-                return;
+                _pageCache.Remove(GetCacheKey(page.Id, page.DataSourceId));
             }
-
-            _pageCache.Remove(GetCacheKey(page.Id, page.DataSourceId));
         }
 
 
@@ -368,13 +364,10 @@ namespace Composite.Data
 
         private static void OnPagePlaceholderChanged(object sender, DataEventArgs args)
         {
-            var placeHolder = args.Data as IPagePlaceholderContent;
-            if (placeHolder == null)
+            if (args.Data is IPagePlaceholderContent placeHolder)
             {
-                return;
+                _placeholderCache.Remove(GetCacheKey(placeHolder.PageId, placeHolder.VersionId, placeHolder.DataSourceId));
             }
-
-            _placeholderCache.Remove(GetCacheKey(placeHolder.PageId, placeHolder.VersionId, placeHolder.DataSourceId));
         }
 
 
@@ -390,14 +383,11 @@ namespace Composite.Data
 
         private static void OnPageStructureChanged(object sender, DataEventArgs args)
         {
-            var ps = args.Data as IPageStructure;
-            if (ps == null)
+            if (args.Data is IPageStructure pageStructure)
             {
-                return;
+                _pageStructureCache.Remove(pageStructure.Id);
+                _childrenCache.Remove(pageStructure.ParentId);
             }
-
-            _pageStructureCache.Remove(ps.Id);
-            _childrenCache.Remove(ps.ParentId);
         }
 
         private static IPage CreateWrapper(IPage page)
