50fd38289041bd66d80dd6b6874865a4e1f40f7d
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2018-01-25 13:43:37 +0100

    Fixing nightwatch tests - tests are failing in scenarios when a button with the same name should be clicked consequently (f.e. the "Next" button)

--- Website/test/e2e/commands/clickDialogButton.js
+++ Website/test/e2e/commands/clickDialogButton.js
@@ -9,6 +9,7 @@ util.inherits(ClickOnDialogButton, events.EventEmitter);
 
 ClickOnDialogButton.prototype.command = function (button) {
     this.client.api
+        .pause(this.api.globals.timeouts.smallest)
         .selectFrame('clickbutton[label="' + button + '"]')
         .waitForElementVisible('clickbutton[label="' + button + '"]', this.api.globals.timeouts.basic)
         .click('clickbutton[label="'+button+'"]', () => this.emit('complete'))
