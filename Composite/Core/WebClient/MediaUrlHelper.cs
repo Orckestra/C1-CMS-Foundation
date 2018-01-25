b0dfa5cbfbdb5ff7c04ce48316f4ba288582d309
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2018-01-25 11:13:28 +0100

    Refactoring

--- Composite/Core/WebClient/MediaUrlHelper.cs
+++ Composite/Core/WebClient/MediaUrlHelper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using Composite.Core.Extensions;
 using Composite.Core.Routing;
 using Composite.Core.Types;
@@ -18,61 +17,53 @@ namespace Composite.Core.WebClient
     /// <summary>    
     /// </summary>
     /// <exclude />
-    [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)] 
-	public static class MediaUrlHelper
-	{
-	    private static readonly string DefaultMediaStore = "MediaArchive";
-        private static readonly Regex GuidRegex = new Regex(@"^(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}$");
+    [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+    public static class MediaUrlHelper
+    {
+        private static readonly string DefaultMediaStore = "MediaArchive";
 
-	    private static readonly int MediaFileCacheSize = 2000;
+        private static readonly int MediaFileCacheSize = 2000;
         private static readonly Cache<string, ExtendedNullable<IMediaFile>> _mediaFileCache = new Cache<string, ExtendedNullable<IMediaFile>>("Media files", MediaFileCacheSize);
 
-	    static MediaUrlHelper()
-	    {
-	        void OnMediaFileChanged(object sender, DataEventArgs args)
-	        {
-	            if (args.Data is IMediaFile mediaFile)
-	            {
-	                _mediaFileCache.Remove(GetCacheKey(mediaFile.StoreId, mediaFile.Id));
-	            }
-	            if (args.Data is IMediaFileData mediaFileData)
-	            {
-	                _mediaFileCache.Remove(GetCacheKey(DefaultMediaStore, mediaFileData.Id));
-	            }
+        static MediaUrlHelper()
+        {
+            void OnMediaFileChanged(object sender, DataEventArgs args)
+            {
+                if (args.Data is IMediaFile mediaFile)
+                {
+                    _mediaFileCache.Remove(GetCacheKey(mediaFile.StoreId, mediaFile.Id));
+                }
+                if (args.Data is IMediaFileData mediaFileData)
+                {
+                    _mediaFileCache.Remove(GetCacheKey(DefaultMediaStore, mediaFileData.Id));
+                }
             }
 
-	        void OnStoreChanged(object sender, StoreEventArgs storeEventArgs)
-	        {
-	            if (!storeEventArgs.DataEventsFired)
-	            {
-	                _mediaFileCache.Clear();
-	            }
+            void OnStoreChanged(object sender, StoreEventArgs storeEventArgs)
+            {
+                if (!storeEventArgs.DataEventsFired)
+                {
+                    _mediaFileCache.Clear();
+                }
             }
 
-	        DataEvents<IMediaFile>.OnAfterAdd += OnMediaFileChanged;
-	        DataEvents<IMediaFile>.OnAfterUpdate += OnMediaFileChanged;
-	        DataEvents<IMediaFile>.OnDeleted += OnMediaFileChanged;
-	        DataEvents<IMediaFile>.OnStoreChanged += OnStoreChanged;
+            DataEvents<IMediaFile>.OnAfterAdd += OnMediaFileChanged;
+            DataEvents<IMediaFile>.OnAfterUpdate += OnMediaFileChanged;
+            DataEvents<IMediaFile>.OnDeleted += OnMediaFileChanged;
+            DataEvents<IMediaFile>.OnStoreChanged += OnStoreChanged;
 
             DataEvents<IMediaFileData>.OnAfterAdd += OnMediaFileChanged;
-	        DataEvents<IMediaFileData>.OnAfterUpdate += OnMediaFileChanged;
-	        DataEvents<IMediaFileData>.OnDeleted += OnMediaFileChanged;
-	        DataEvents<IMediaFileData>.OnStoreChanged += OnStoreChanged;
-	    }
+            DataEvents<IMediaFileData>.OnAfterUpdate += OnMediaFileChanged;
+            DataEvents<IMediaFileData>.OnDeleted += OnMediaFileChanged;
+            DataEvents<IMediaFileData>.OnStoreChanged += OnStoreChanged;
+        }
 
         /// <exclude />
-        public static string GetUrl(IMediaFile file)
-        {
-            return GetUrl(file, true, false);
-        }
+        public static string GetUrl(IMediaFile file) => GetUrl(file, true, false);
 
 
         /// <exclude />
-        public static string GetUrl(IMediaFile file, bool isInternal)
-        {
-            return GetUrl(file, isInternal, false);
-        }
-
+        public static string GetUrl(IMediaFile file, bool isInternal) => GetUrl(file, isInternal, false);
 
 
         /// <exclude />
@@ -82,10 +73,12 @@ namespace Composite.Core.WebClient
 
             if (!downloadableMedia) return url;
 
-            var urlBuilder = new UrlBuilder(url);
-            urlBuilder["download"] = "true";
+            var urlBuilder = new UrlBuilder(url)
+            {
+                ["download"] = "true"
+            };
 
-            return  urlBuilder.ToString();
+            return urlBuilder.ToString();
         }
 
 
@@ -93,16 +86,16 @@ namespace Composite.Core.WebClient
         /// <exclude />
         public static IMediaFile GetFileFromQueryString(NameValueCollection queryParameters)
         {
-            Verify.ArgumentNotNull(queryParameters, "queryParameters");
+            Verify.ArgumentNotNull(queryParameters, nameof(queryParameters));
 
             string idStr = queryParameters["id"];
 
             // In order to support old-style queries, checking composite path in "i" and "src" query parameters
             string compositePath = queryParameters["i"];
-            if(compositePath.IsNullOrEmpty())
+            if (compositePath.IsNullOrEmpty())
             {
                 compositePath = queryParameters["src"];
-                if(compositePath.IsNullOrEmpty() 
+                if (compositePath.IsNullOrEmpty()
                     && idStr != null && idStr.Contains(":"))
                 {
                     compositePath = idStr;
@@ -111,8 +104,8 @@ namespace Composite.Core.WebClient
 
             string storeId;
             IMediaFile result;
-            
-            if(!compositePath.IsNullOrEmpty())
+
+            if (!compositePath.IsNullOrEmpty())
             {
                 // Parsing a friendly media url
                 int separatorIndex = compositePath.IndexOf(":", System.StringComparison.Ordinal);
@@ -123,9 +116,7 @@ namespace Composite.Core.WebClient
 
                 try
                 {
-                    Guid fileId; 
-
-                    if (Guid.TryParse(secondPart, out fileId))
+                    if (Guid.TryParse(secondPart, out Guid fileId))
                     {
                         result = GetFileById(storeId, fileId);
                     }
@@ -138,7 +129,7 @@ namespace Composite.Core.WebClient
                 }
                 catch (Exception ex)
                 {
-                    throw new FileNotFoundException("File '{0}' was not found.".FormatWith(compositePath), ex);
+                    throw new FileNotFoundException($"File '{compositePath}' was not found.", ex);
                 }
             }
             else
@@ -159,13 +150,11 @@ namespace Composite.Core.WebClient
                 }
                 catch (Exception ex)
                 {
-                    throw new FileNotFoundException("File not found. Storage: '{0}', Id: {1}".FormatWith(storeId, id), ex);
+                    throw new FileNotFoundException($"File not found. Storage: '{storeId}', Id: {id}", ex);
                 }
             }
 
-            if (result == null) throw new FileNotFoundException("File not found.");
-
-            return result;
+            return result ?? throw new FileNotFoundException("File not found.");
         }
 
         internal static string GetCacheKey(string storeId, Guid fileId) => storeId + fileId;
@@ -215,17 +204,13 @@ namespace Composite.Core.WebClient
 
         /// <exclude />
         [Obsolete("Use Guid.TryParse()")]
-        public static bool IsValidGuid(string value)
-        {
-            Verify.ArgumentNotNull(value, "value");
+        public static bool IsValidGuid(string value) => Guid.TryParse(value, out _);
 
-            return GuidRegex.IsMatch(value);
-        }
 
         /// <exclude />
         public static string ChangeInternalMediaUrlsToPublic(string content)
         {
-            return InternalUrls.ConvertInternalUrlsToPublic(content, new[] {new MediaInternalUrlConverter()});
+            return InternalUrls.ConvertInternalUrlsToPublic(content, new[] { new MediaInternalUrlConverter() });
         }
-	}
+    }
 }
