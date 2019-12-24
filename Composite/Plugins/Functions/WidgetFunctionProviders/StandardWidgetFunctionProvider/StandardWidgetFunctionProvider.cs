d7debff3d5d65a6eedc631ff7b2ae989fef2bcbe
Igor Dovgaliuk <igor.dovgaliuk@orckestra.com>
2019-12-24 12:22:10 +0200

    removed home page selector from widgets list

--- Composite/Plugins/Functions/WidgetFunctionProviders/StandardWidgetFunctionProvider/StandardWidgetFunctionProvider.cs
+++ Composite/Plugins/Functions/WidgetFunctionProviders/StandardWidgetFunctionProvider/StandardWidgetFunctionProvider.cs
@@ -107,7 +107,6 @@ namespace Composite.Plugins.Functions.WidgetFunctionProviders.StandardWidgetFunc
             _widgetStaticTypeFunctions.Add(new CheckBoxWidgetFuntion(_entityTokenFactory));
             _widgetStaticTypeFunctions.Add(new BoolSelectorWidgetFuntion(_entityTokenFactory));
             _widgetStaticTypeFunctions.Add(new DataTypeSelectorWidgetFunction(_entityTokenFactory));
-            _widgetStaticTypeFunctions.Add(new HomePageSelectorWidgetFunction(_entityTokenFactory));
             _widgetStaticTypeFunctions.Add(new PageReferenceSelectorWidgetFunction(_entityTokenFactory));
             _widgetStaticTypeFunctions.Add(new NullablePageReferenceSelectorWidgetFunction(_entityTokenFactory));
             _widgetStaticTypeFunctions.Add(new FontIconSelectorWidgetFuntion(_entityTokenFactory));
