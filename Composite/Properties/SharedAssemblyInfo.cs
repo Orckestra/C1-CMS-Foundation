9a0bf1de738a45ba085a98acfb68aedf73e948d8
Dmitry Dzygin <dmitry.dzygin@orckestra.com>
2021-11-01 15:22:45 +0100

    Updating version number to 6.11

--- Composite/Properties/SharedAssemblyInfo.cs
+++ Composite/Properties/SharedAssemblyInfo.cs
@@ -2,15 +2,15 @@ using System.Reflection;
 
 // General Information about the assemblies Composite and Composite.Workflows  
 #if !InternalBuild
-[assembly: AssemblyTitle("C1 CMS 6.10")]
+[assembly: AssemblyTitle("C1 CMS 6.11")]
 #else
-[assembly: AssemblyTitle("C1 CMS 6.10 (Internal Build)")]
+[assembly: AssemblyTitle("C1 CMS 6.11 (Internal Build)")]
 #endif
 
 [assembly: AssemblyCompany("Orckestra Technologies Inc.")]
 [assembly: AssemblyProduct("C1 CMS")]
-[assembly: AssemblyCopyright("Copyright © Orckestra Technologies Inc. 2020")]
+[assembly: AssemblyCopyright("Copyright © Orckestra Technologies Inc. 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("6.10.*")]
+[assembly: AssemblyVersion("6.11.*")]
